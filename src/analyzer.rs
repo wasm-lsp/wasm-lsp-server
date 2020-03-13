7669f9f28793857b3b07815d47eaf4977c99708e
silvanshade <silvanshade@users.noreply.github.com>
2020-03-13 12:21:54 -0600

    Define Analyzer struct

---
+++ src/analyzer.rs
@@ -0,0 +1,13 @@
+use failure::Fallible;
+use std::sync::{Arc, Mutex};
+use tree_sitter::Parser;
+
+pub struct Analyzer {
+    pub parser: Arc<Mutex<Parser>>,
+}
+
+impl Analyzer {
+    pub fn new(parser: Arc<Mutex<Parser>>) -> Fallible<Analyzer> {
+        Ok(Analyzer { parser })
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod analyzer;
 mod error;
 mod parser;
 mod server;
