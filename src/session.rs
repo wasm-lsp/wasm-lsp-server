f71f468907f98aee7eb90b752789ebd8036ce7ec
silvanshade <silvanshade@users.noreply.github.com>
2020-03-13 12:22:15 -0600

    Initialize analyzer for session

--- src/session.rs
+++ src/session.rs
@@ -1,15 +1,17 @@
-use crate::parser;
+use crate::{analyzer::Analyzer, parser};
 use failure::Fallible;
 use std::sync::{Arc, Mutex};
 use tree_sitter::Parser;
 
 pub struct Session {
+    pub analyzer: Arc<Analyzer>,
     pub parser: Arc<Mutex<Parser>>,
 }
 
 impl Session {
     pub fn new() -> Fallible<Session> {
         let parser = Arc::new(Mutex::new(parser::Factory::parser()?));
-        Ok(Session { parser })
+        let analyzer = Arc::new(Analyzer::new(parser.clone())?);
+        Ok(Session { analyzer, parser })
     }
 }
