ab956cde7852b782084497473ff79905b8963a46
silvanshade <silvanshade@users.noreply.github.com>
2020-03-12 19:57:29 -0600

    Begin implementing server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use jsonrpc_core::Result;
+use tower_lsp::lsp_types::*;
+use tower_lsp::{Client, LanguageServer, LspService, Server};
+
+#[derive(Debug, Default)]
+struct Backend;
+
+#[tower_lsp::async_trait]
+impl LanguageServer for Backend {
+    fn initialize(&self, _: &Client, _: InitializeParams) -> Result<InitializeResult> {
+        Ok(InitializeResult::default())
+    }
+
+    async fn initialized(&self, client: &Client, _: InitializedParams) {
+        client.log_message(MessageType::Info, "server initialized!");
+    }
+
+    async fn shutdown(&self) -> Result<()> {
+        Ok(())
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let stdin = tokio::io::stdin();
+    let stdout = tokio::io::stdout();
+    let (service, messages) = LspService::new(Backend::default());
+    Server::new(stdin, stdout)
+        .interleave(messages)
+        .serve(service)
+        .await;
 }
