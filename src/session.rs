8a7ee52fbf2885450f2d9dc2f72fb1aec14d5691
silvanshade <silvanshade@users.noreply.github.com>
2020-03-13 12:20:33 -0600

    Initialize tree sitter parser for session

--- src/session.rs
+++ src/session.rs
@@ -1,9 +1,15 @@
+use crate::parser;
 use failure::Fallible;
+use std::sync::{Arc, Mutex};
+use tree_sitter::Parser;
 
-pub struct Session;
+pub struct Session {
+    pub parser: Arc<Mutex<Parser>>,
+}
 
 impl Session {
     pub fn new() -> Fallible<Session> {
-        Ok(Session)
+        let parser = Arc::new(Mutex::new(parser::Factory::parser()?));
+        Ok(Session { parser })
     }
 }
