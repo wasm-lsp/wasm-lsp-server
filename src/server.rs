eb4f9595eafca19b3893fe34ea1aa9e815ca0b23
Darin Morrison <darinmorrison@users.noreply.github.com>
2020-03-13 23:21:03 -0600

    Set capabilities to respond to workspace changes

--- src/server.rs
+++ src/server.rs
@@ -5,7 +5,20 @@ use tower_lsp::{lsp_types::*, Client, LanguageServer};
 #[tower_lsp::async_trait]
 impl LanguageServer for Session {
     fn initialize(&self, _: &Client, _: InitializeParams) -> Result<InitializeResult> {
-        Ok(InitializeResult::default())
+        Ok(InitializeResult {
+            capabilities: ServerCapabilities {
+                workspace: Some(WorkspaceCapability {
+                    workspace_folders: Some(WorkspaceFolderCapability {
+                        supported: Some(true),
+                        change_notifications: Some(
+                            WorkspaceFolderCapabilityChangeNotifications::Bool(true),
+                        ),
+                    }),
+                }),
+                ..ServerCapabilities::default()
+            },
+            ..InitializeResult::default()
+        })
     }
 
     async fn initialized(&self, client: &Client, _: InitializedParams) {
