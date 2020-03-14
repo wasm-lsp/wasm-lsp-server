eb0704645e2e13c9b933de3af8257c86c040971b
Darin Morrison <darinmorrison@users.noreply.github.com>
2020-03-14 16:09:08 -0600

    Open a new sled database on session initialize

--- src/session.rs
+++ src/session.rs
@@ -1,17 +1,23 @@
-use crate::{analyzer::Analyzer, parser};
+use crate::{analyzer::Analyzer, database::Database, parser};
 use failure::Fallible;
 use std::sync::{Arc, Mutex};
 use tree_sitter::Parser;
 
 pub struct Session {
     analyzer: Arc<Analyzer>,
+    database: Arc<Database>,
     parser: Arc<Mutex<Parser>>,
 }
 
 impl Session {
     pub fn new() -> Fallible<Session> {
+        let database = Arc::new(Database::new()?);
         let parser = Arc::new(Mutex::new(parser::Factory::parser()?));
-        let analyzer = Arc::new(Analyzer::new(parser.clone())?);
-        Ok(Session { analyzer, parser })
+        let analyzer = Arc::new(Analyzer::new(database.clone(), parser.clone())?);
+        Ok(Session {
+            analyzer,
+            database,
+            parser,
+        })
     }
 }
