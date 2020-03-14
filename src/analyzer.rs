d48db3d4dd6ad2e94879b2a8989aa38008944d48
silvanshade <silvanshade@users.noreply.github.com>
2020-03-14 16:09:08 -0600

    Open a new sled database on session initialize

--- src/analyzer.rs
+++ src/analyzer.rs
@@ -1,13 +1,15 @@
+use crate::database::Database;
 use failure::Fallible;
 use std::sync::{Arc, Mutex};
 use tree_sitter::Parser;
 
 pub struct Analyzer {
+    database: Arc<Database>,
     parser: Arc<Mutex<Parser>>,
 }
 
 impl Analyzer {
-    pub fn new(parser: Arc<Mutex<Parser>>) -> Fallible<Analyzer> {
-        Ok(Analyzer { parser })
+    pub fn new(database: Arc<Database>, parser: Arc<Mutex<Parser>>) -> Fallible<Analyzer> {
+        Ok(Analyzer { database, parser })
     }
 }
