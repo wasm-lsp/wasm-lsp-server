0b229325a066131577b101cb059197054425e924
silvanshade <silvanshade@users.noreply.github.com>
2020-03-15 13:28:11 -0600

    Modify build script to use parser submodule

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 use std::path::Path;
 
 fn main() {
-    let dir = Path::new("../tree-sitter-webassembly/src");
+    let dir = Path::new("vendor/tree-sitter-webassembly/src");
     cc::Build::new()
         .include(&dir)
         .file(dir.join("parser.c"))
