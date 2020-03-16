82d9d4bff5bd62c3f838cc984a21536634104812
Darin Morrison <darinmorrison@users.noreply.github.com>
2020-03-16 14:41:38 -0600

    Remove explicit borrows

--- build.rs
+++ build.rs
@@ -3,19 +3,19 @@ use std::path::Path;
 fn main() {
     let dir = Path::new("vendor/tree-sitter-webassembly");
     cc::Build::new()
-        .include(&dir.join("wast/src"))
+        .include(dir.join("wast/src"))
         .file(dir.join("wast/src/parser.c"))
         .compile("tree-sitter-wast");
     cc::Build::new()
-        .include(&dir.join("wat/src"))
+        .include(dir.join("wat/src"))
         .file(dir.join("wat/src/parser.c"))
         .compile("tree-sitter-wat");
     cc::Build::new()
-        .include(&dir.join("wit/src"))
+        .include(dir.join("wit/src"))
         .file(dir.join("wit/src/parser.c"))
         .compile("tree-sitter-wit");
     cc::Build::new()
-        .include(&dir.join("witx/src"))
+        .include(dir.join("witx/src"))
         .file(dir.join("witx/src/parser.c"))
         .compile("tree-sitter-witx");
 }
