e4ffee26495c6b8f0207e1c974ea1e40b609d5ac
silvanshade <silvanshade@users.noreply.github.com>
2020-03-17 15:37:28 -0600

    Add TODO about highlighting and semantic tokens

--- src/highlighter.rs
+++ src/highlighter.rs
@@ -2,6 +2,8 @@ use crate::synchronizer::Synchronizer;
 use failure::Fallible;
 use std::sync::Arc;
 
+// TODO: implement highlight adapter from tree-sitter to LSP SemanticTokenType
+
 /// Computes highlights from elaborated syntax and metadata in
 /// [`Database`](crate::database::Database).
 pub struct Highlighter {
