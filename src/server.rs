693f2a5423c319e2ad521b5a2571113fccf1373b
silvanshade <silvanshade@users.noreply.github.com>
2020-03-20 15:21:35 -0600

    Refactor capabilities

--- src/server.rs
+++ src/server.rs
@@ -7,39 +7,44 @@ use tower_lsp::{lsp_types::*, Client, LanguageServer};
 impl LanguageServer for Session {
     fn initialize(&self, _: &Client, data: InitializeParams) -> Result<InitializeResult> {
         log::info!("{:?}", data);
-        let capabilities = ServerCapabilities {
-            document_symbol_provider: Some(true),
-            semantic_tokens_provider: Some(SemanticTokensServerCapabilities::SemanticTokensRegistrationOptions(
-                SemanticTokensRegistrationOptions {
-                    semantic_tokens_options: SemanticTokensOptions {
-                        document_provider: Some(SemanticTokensDocumentProvider::Bool(true)),
-                        legend: SemanticTokensLegend {
-                            token_modifiers: vec![],
-                            token_types: vec![],
-                        },
-                        range_provider: Some(false),
-                        work_done_progress_options: WorkDoneProgressOptions {
-                            work_done_progress: Some(false),
-                        },
+        let document_symbol_provider = Some(true);
+        let semantic_tokens_provider = Some(SemanticTokensServerCapabilities::SemanticTokensRegistrationOptions(
+            SemanticTokensRegistrationOptions {
+                semantic_tokens_options: SemanticTokensOptions {
+                    document_provider: Some(SemanticTokensDocumentProvider::Bool(true)),
+                    legend: SemanticTokensLegend {
+                        token_modifiers: vec![],
+                        token_types: vec![],
                     },
-                    static_registration_options: StaticRegistrationOptions { id: None },
-                    text_document_registration_options: TextDocumentRegistrationOptions {
-                        document_selector: None,
+                    range_provider: Some(false),
+                    work_done_progress_options: WorkDoneProgressOptions {
+                        work_done_progress: Some(false),
                     },
                 },
-            )),
-            text_document_sync: Some(TextDocumentSyncCapability::Options(TextDocumentSyncOptions {
-                open_close: Some(true),
-                change: Some(TextDocumentSyncKind::Full),
-                ..TextDocumentSyncOptions::default()
-            })),
-            workspace: Some(WorkspaceCapability {
-                workspace_folders: Some(WorkspaceFolderCapability {
-                    supported: Some(true),
-                    change_notifications: Some(WorkspaceFolderCapabilityChangeNotifications::Bool(true)),
-                }),
+                static_registration_options: StaticRegistrationOptions { id: None },
+                text_document_registration_options: TextDocumentRegistrationOptions {
+                    document_selector: None,
+                },
+            },
+        ));
+        let text_document_sync = Some(TextDocumentSyncCapability::Options(TextDocumentSyncOptions {
+            open_close: Some(true),
+            change: Some(TextDocumentSyncKind::Full),
+            ..TextDocumentSyncOptions::default()
+        }));
+        let workspace = Some(WorkspaceCapability {
+            workspace_folders: Some(WorkspaceFolderCapability {
+                supported: Some(true),
+                change_notifications: Some(WorkspaceFolderCapabilityChangeNotifications::Bool(true)),
             }),
-            workspace_symbol_provider: Some(true),
+        });
+        let workspace_symbol_provider = Some(true);
+        let capabilities = ServerCapabilities {
+            document_symbol_provider,
+            semantic_tokens_provider,
+            text_document_sync,
+            workspace,
+            workspace_symbol_provider,
             ..ServerCapabilities::default()
         };
         Ok(InitializeResult {
