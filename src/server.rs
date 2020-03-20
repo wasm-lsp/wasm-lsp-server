a5f55a159e96c8ac926e5c9a3d313c580447440e
Darin Morrison <darinmorrison@users.noreply.github.com>
2020-03-20 16:27:07 -0600

    Comment out most capabilities settings for now

--- src/server.rs
+++ src/server.rs
@@ -5,46 +5,46 @@ use tower_lsp::{lsp_types::*, Client, LanguageServer};
 
 #[tower_lsp::async_trait]
 impl LanguageServer for Session {
-    fn initialize(&self, _: &Client, data: InitializeParams) -> Result<InitializeResult> {
-        log::info!("{:?}", data);
-        let document_symbol_provider = Some(true);
-        let semantic_tokens_provider = Some(SemanticTokensServerCapabilities::SemanticTokensRegistrationOptions(
-            SemanticTokensRegistrationOptions {
-                semantic_tokens_options: SemanticTokensOptions {
-                    document_provider: Some(SemanticTokensDocumentProvider::Bool(false)),
-                    legend: SemanticTokensLegend {
-                        token_modifiers: vec![],
-                        token_types: vec![],
-                    },
-                    range_provider: Some(false),
-                    work_done_progress_options: WorkDoneProgressOptions {
-                        work_done_progress: Some(false),
-                    },
-                },
-                static_registration_options: StaticRegistrationOptions { id: None },
-                text_document_registration_options: TextDocumentRegistrationOptions {
-                    document_selector: None,
-                },
-            },
-        ));
+    fn initialize(&self, _: &Client, params: InitializeParams) -> Result<InitializeResult> {
+        log::info!("{:?}", params);
+        // let document_symbol_provider = Some(false);
+        // let semantic_tokens_provider = Some(SemanticTokensServerCapabilities::SemanticTokensRegistrationOptions(
+        //     SemanticTokensRegistrationOptions {
+        //         semantic_tokens_options: SemanticTokensOptions {
+        //             document_provider: Some(SemanticTokensDocumentProvider::Bool(false)),
+        //             legend: SemanticTokensLegend {
+        //                 token_modifiers: vec![],
+        //                 token_types: vec![],
+        //             },
+        //             range_provider: Some(false),
+        //             work_done_progress_options: WorkDoneProgressOptions {
+        //                 work_done_progress: Some(false),
+        //             },
+        //         },
+        //         static_registration_options: StaticRegistrationOptions { id: None },
+        //         text_document_registration_options: TextDocumentRegistrationOptions {
+        //             document_selector: None,
+        //         },
+        //     },
+        // ));
         let text_document_sync = Some(TextDocumentSyncCapability::Options(TextDocumentSyncOptions {
             open_close: Some(true),
             change: Some(TextDocumentSyncKind::Full),
             ..TextDocumentSyncOptions::default()
         }));
-        let workspace = Some(WorkspaceCapability {
-            workspace_folders: Some(WorkspaceFolderCapability {
-                supported: Some(false),
-                change_notifications: Some(WorkspaceFolderCapabilityChangeNotifications::Bool(false)),
-            }),
-        });
-        let workspace_symbol_provider = Some(false);
+        // let workspace = Some(WorkspaceCapability {
+        //     workspace_folders: Some(WorkspaceFolderCapability {
+        //         supported: Some(false),
+        //         change_notifications: Some(WorkspaceFolderCapabilityChangeNotifications::Bool(false)),
+        //     }),
+        // });
+        // let workspace_symbol_provider = Some(false);
         let capabilities = ServerCapabilities {
-            document_symbol_provider,
-            semantic_tokens_provider,
+            // document_symbol_provider,
+            // semantic_tokens_provider,
             text_document_sync,
-            workspace,
-            workspace_symbol_provider,
+            // workspace,
+            // workspace_symbol_provider,
             ..ServerCapabilities::default()
         };
         Ok(InitializeResult {
