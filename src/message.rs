1345e3a6b5ae2d06850f5f07b0542f4d8e5f4e46
Darin Morrison <darinmorrison@users.noreply.github.com>
2020-03-22 19:57:24 -0600

    Refactoring

--- src/message.rs
+++ src/message.rs
@@ -4,8 +4,8 @@ use tower_lsp::Client;
 /// Messages for synchronizing server component activity.
 #[derive(Clone, Debug)]
 pub enum Message {
+    Start,
     TreeDidChange { client: Client, uri: Url },
     TreeDidClose { client: Client, uri: Url },
     TreeDidOpen { client: Client, uri: Url },
-    Start,
 }
