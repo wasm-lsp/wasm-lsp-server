56d92dc0fae5cb7cd89235370aeaebd206fc4295
silvanshade <silvanshade@users.noreply.github.com>
2020-03-22 18:19:25 -0600

    Begin handling tree change events with Elaborator

--- src/elaborator.rs
+++ src/elaborator.rs
@@ -1,7 +1,9 @@
 use crate::{database::Database, message::Message, synchronizer::Synchronizer};
 use failure::Fallible;
+use lsp_types::*;
 use std::sync::Arc;
 use tokio::sync::watch;
+use tower_lsp::Client;
 
 /// Elaborates a given [`Tree`] into structured data to be cached in
 /// [`Database`](crate::database::Database).
@@ -28,8 +30,41 @@ impl Elaborator {
 
     pub async fn init(&self) -> Fallible<()> {
         let mut rx = self.rx.clone();
-        while let Some(_value) = rx.recv().await {
-            log::info!("{:?}", rx);
+        while let Some(message) = rx.recv().await {
+            match message {
+                Message::TreeDidChange { client, uri, .. } => self.tree_did_change(client, uri).await?,
+                Message::TreeDidClose { client, uri, .. } => self.tree_did_close(client, uri).await?,
+                Message::TreeDidOpen { client, uri, .. } => self.tree_did_open(client, uri).await?,
+                _ => {},
+            }
+        }
+        Ok(())
+    }
+
+    async fn tree_did_change(&self, _: Client, uri: Url) -> Fallible<()> {
+        if let Some(tree) = self.synchronizer.trees.get(&uri) {
+            let tree = tree.lock().await.clone();
+            let node = tree.root_node();
+            if !node.has_error() {
+                log::info!("syntax well-formed");
+            }
+            // NOTE: else let auditor handle
+        }
+        Ok(())
+    }
+
+    async fn tree_did_close(&self, _: Client, _: Url) -> Fallible<()> {
+        Ok(())
+    }
+
+    async fn tree_did_open(&self, _: Client, uri: Url) -> Fallible<()> {
+        if let Some(tree) = self.synchronizer.trees.get(&uri) {
+            let tree = tree.lock().await.clone();
+            let node = tree.root_node();
+            if !node.has_error() {
+                log::info!("syntax well-formed");
+            }
+            // NOTE: else let auditor handle
         }
         Ok(())
     }
