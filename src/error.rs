bbe103d34347e79d2e1e88b1563bfce99e6e9e55
Darin Morrison <darinmorrison@users.noreply.github.com>
2020-03-23 19:59:15 -0600

    Start responding to document symbol requests

--- src/error.rs
+++ src/error.rs
@@ -7,4 +7,6 @@ pub enum Error {
     LanguageError(tree_sitter::LanguageError),
     #[fail(display = "tree_sitter::QueryError")]
     QueryError(tree_sitter::QueryError),
+    #[fail(display = "{}", 0)]
+    Utf8Error(std::str::Utf8Error),
 }
