fad82044427e8306dc24b2aed2b8b71ee2cb74ed
Darin Morrison <darinmorrison@users.noreply.github.com>
2020-03-23 19:56:45 -0600

    Store document text along with trees

--- src/synchronizer.rs
+++ src/synchronizer.rs
@@ -6,38 +6,53 @@ use tokio::sync::Mutex;
 use tower_lsp::Client;
 use tree_sitter::Tree;
 
+pub struct Document {
+    pub text: String,
+    pub tree: Mutex<Tree>,
+}
+
 /// Synchronizes document edits and parse trees and notifies other server components of changes.
 pub struct Synchronizer {
     parser: Parser,
     session: SessionHandle,
-    pub trees: DashMap<Url, Mutex<Tree>>,
+    pub documents: DashMap<Url, Document>,
 }
 
 impl Synchronizer {
     pub fn new(parser: Parser, session: SessionHandle) -> Fallible<Self> {
-        let trees = DashMap::new();
-        Ok(Synchronizer { parser, session, trees })
+        let documents = DashMap::new();
+        Ok(Synchronizer {
+            parser,
+            session,
+            documents,
+        })
     }
 
     pub async fn did_open(&self, client: &Client, params: DidOpenTextDocumentParams) -> Fallible<()> {
-        let mut parser = self.parser.wat.lock().await;
         let DidOpenTextDocumentParams {
             text_document: TextDocumentItem { uri, text, .. },
-        } = &params;
-        let tree;
-        {
-            // NOTE: Perhaps we should persist trees even on close. We could make this configurable.
-            let old_tree = None;
-            tree = parser.parse(text, old_tree);
-        }
-        if let Some(tree) = tree {
-            let _ = self.trees.insert(uri.clone(), Mutex::new(tree));
-            self.session.get().await.auditor.tree_did_open(client, uri).await?;
-            self.session.get().await.elaborator.tree_did_open(client, uri).await?;
-        } else {
-            // TODO: report
-        }
-        Ok(())
+        } = params;
+        let params = {
+            let text_document = {
+                let uri = uri;
+                let version = None;
+                VersionedTextDocumentIdentifier { uri, version }
+            };
+            let content_changes = vec![{
+                let range = None;
+                let range_length = None;
+                TextDocumentContentChangeEvent {
+                    range,
+                    range_length,
+                    text,
+                }
+            }];
+            DidChangeTextDocumentParams {
+                text_document,
+                content_changes,
+            }
+        };
+        self.did_change(client, params).await
     }
 
     pub async fn did_change(&self, client: &Client, params: DidChangeTextDocumentParams) -> Fallible<()> {
@@ -46,13 +61,18 @@ impl Synchronizer {
             text_document: VersionedTextDocumentIdentifier { uri, .. },
             content_changes,
         } = &params;
-        let TextDocumentContentChangeEvent { ref text, .. } = content_changes[0];
+        let TextDocumentContentChangeEvent { text, .. } = &content_changes[0];
         let mut success = false;
         {
             // TODO: Fetch old_tree from cache and apply edits to prepare for incremental re-parsing.
             let old_tree = None;
             if let Some(tree) = parser.parse(text, old_tree) {
-                self.trees.insert(uri.clone(), Mutex::new(tree));
+                let document = {
+                    let text = text.clone();
+                    let tree = Mutex::new(tree);
+                    Document { text, tree }
+                };
+                self.documents.insert(uri.clone(), document);
                 success = true;
             }
         }
@@ -69,7 +89,7 @@ impl Synchronizer {
         let DidCloseTextDocumentParams {
             text_document: TextDocumentIdentifier { uri },
         } = &params;
-        self.trees.remove(&uri);
+        self.documents.remove(uri);
         self.session.get().await.auditor.tree_did_close(client, uri).await?;
         self.session.get().await.elaborator.tree_did_close(client, uri).await?;
         Ok(())
