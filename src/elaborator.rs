d8a257fce69d90abc608031c715255047fdb2a37
silvanshade <silvanshade@users.noreply.github.com>
2020-03-27 01:07:09 -0600

    Rewrite match processor

--- src/elaborator.rs
+++ src/elaborator.rs
@@ -6,32 +6,44 @@ use failure::Fallible;
 use lsp_types::*;
 use std::sync::Arc;
 use tower_lsp::Client;
+use tree_sitter::QueryMatch;
 
-static QUERY: &str = r"
-(module
-    (id) @module)
+mod queries {
+    pub mod document_symbol {
+        pub static QUERY: &str = r"
+        (module
+            (id) @module)
 
-(module
-    (id) @module
-    (modulefield
-        (import
-            (name) @import-module
-            (name) @import-item
-            (importdesc
-                (id) @importdesc))))
+        (module
+            (id) @module
+            (modulefield
+                (import
+                    (name) @import-module
+                    (name) @import-item
+                    (importdesc
+                        (id) @importdesc))))
 
-(module
-    (id) @module
-    (modulefield
-        (type
-            (id) @type)))
+        (module
+            (id) @module
+            (modulefield
+                (type
+                    (id) @type)))
 
-(module
-    (id) @module
-    (modulefield
-        (func
-            (id) @func)))
-";
+        (module
+            (id) @module
+            (modulefield
+                (func
+                    (id) @func)))
+        ";
+
+        pub mod patterns {
+            pub static MODULE: usize = 0;
+            pub static IMPORT: usize = 1;
+            pub static TYPE: usize = 2;
+            pub static FUNC: usize = 3;
+        }
+    }
+}
 
 pub async fn tree_did_change(documents: Arc<DashMap<Url, Document>>, _: &Client, uri: Url) -> Fallible<()> {
     if let Some(document) = documents.get(&uri) {
@@ -72,7 +84,7 @@ pub async fn document_symbol(
         // TODO: allow partial elaboration in presence of syntax errors
         if !node.has_error() {
             // prepare a query to match tree-sitter module nodes
-            let source = QUERY;
+            let source = queries::document_symbol::QUERY;
             let language = tree.language();
             let query = tree_sitter::Query::new(language, source).map_err(|err| {
                 let code = jsonrpc_core::ErrorCode::InternalError;
@@ -86,136 +98,94 @@ pub async fn document_symbol(
             let text_callback = |node: tree_sitter::Node| &document.text[node.byte_range()];
             let matches = query_cursor.captures(&query, node, text_callback);
 
-            // FIXME: make this less redundant
+            // prepare the stack machine
+            let mut stack: [Vec<&str>; 4] = [vec![], vec![], vec![], vec![]];
+            let mut state: [u8; 4] = [0, 0, 0, 0];
 
-            let mut data1 = Vec::<&str>::new();
-            let mut data2 = Vec::<&str>::new();
-            let mut data3 = Vec::<&str>::new();
-
-            let mut state1 = 0u8;
-            let mut state2 = 0u8;
-            let mut state3 = 0u8;
-
-            for (mat, index) in matches {
-                let capture = mat.captures[index];
+            for (
+                QueryMatch {
+                    captures,
+                    pattern_index,
+                    ..
+                },
+                index,
+            ) in matches
+            {
+                let capture = captures[index];
                 let text = capture
                     .node
                     .utf8_text(&document.text.as_bytes())
                     .unwrap_or("")
                     .trim_start();
+
+                stack[pattern_index].push(text);
+                state[pattern_index] += 1;
+
                 log::info!(
                     "pattern: {}, capture: {}, row: {}, text: {:?}",
-                    mat.pattern_index,
+                    pattern_index,
                     &query.capture_names()[capture.index as usize],
                     capture.node.start_position().row,
                     text
                 );
 
-                if mat.pattern_index == 0 {
-                    let start = node.start_position();
-                    let end = node.end_position();
+                if pattern_index == queries::document_symbol::patterns::MODULE {
                     results.push({
-                        let name = String::from(text);
-                        let deprecated = Default::default();
-                        let location = {
-                            let uri = uri.clone();
-                            let range = {
-                                let start = Position::new(start.row as u64, start.column as u64);
-                                let end = Position::new(end.row as u64, end.column as u64);
-                                Range::new(start, end)
-                            };
-                            Location { uri, range }
-                        };
-                        let container_name = Default::default();
-                        let kind = SymbolKind::Module;
                         SymbolInformation {
-                            name,
-                            kind,
-                            deprecated,
-                            location,
-                            container_name,
+                            name: String::from(text),
+                            kind: SymbolKind::Module,
+                            deprecated: Default::default(),
+                            location: crate::lsp::node::location(uri.clone(), &node),
+                            container_name: Default::default(),
                         }
                     });
                 }
 
-                if mat.pattern_index == 1 {
-                    data1.push(text);
-                    state1 += 1;
-                    if state1.wrapping_rem_euclid(4) == 0 {
-                        if let [module, import_module, import_item, importdesc] = data1.drain(.. 4).collect::<Vec<_>>()[..] {
-                            log::info!("{}, {}, {}, {}", module, import_module, import_item, importdesc);
-                        }
+                if pattern_index == queries::document_symbol::patterns::IMPORT
+                    && state[pattern_index].wrapping_rem_euclid(4) == 0
+                {
+                    if let [module, import_module, import_item, importdesc] =
+                        stack[pattern_index].drain(.. 4).collect::<Vec<_>>()[..]
+                    {
+                        log::info!("{}, {}, {}, {}", module, import_module, import_item, importdesc);
                     }
                 }
 
-                if mat.pattern_index == 2 {
-                    data2.push(text);
-                    state2 += 1;
-                    if state2.wrapping_rem_euclid(2) == 0 {
-                        if let [module, type_] = data2.drain(.. 2).collect::<Vec<_>>()[..] {
-                            log::info!("{}, {}", module, type_);
-                            let start = node.start_position();
-                            let end = node.end_position();
-                            results.push({
-                                let name = String::from(type_);
-                                let deprecated = None;
-                                let location = {
-                                    let uri = uri.clone();
-                                    let range = {
-                                        let start = Position::new(start.row as u64, start.column as u64);
-                                        let end = Position::new(end.row as u64, end.column as u64);
-                                        Range::new(start, end)
-                                    };
-                                    Location { uri, range }
-                                };
-                                let container_name = Some(String::from(module));
-                                let kind = SymbolKind::TypeParameter;
-                                SymbolInformation {
-                                    name,
-                                    kind,
-                                    deprecated,
-                                    location,
-                                    container_name,
-                                }
-                            });
-                        }
+                if pattern_index == queries::document_symbol::patterns::TYPE
+                    && state[pattern_index].wrapping_rem_euclid(2) == 0
+                {
+                    if let [module, type_] = stack[pattern_index].drain(.. 2).collect::<Vec<_>>()[..] {
+                        log::info!("{}, {}", module, type_);
+                        results.push({
+                            SymbolInformation {
+                                name: String::from(type_),
+                                kind: SymbolKind::TypeParameter,
+                                deprecated: Default::default(),
+                                location: crate::lsp::node::location(uri.clone(), &node),
+                                container_name: Some(String::from(module)),
+                            }
+                        });
                     }
                 }
 
-                if mat.pattern_index == 3 {
-                    data3.push(text);
-                    state3 += 1;
-                    if state3.wrapping_rem_euclid(2) == 0 {
-                        if let [module, func] = data3.drain(.. 2).collect::<Vec<_>>()[..] {
-                            log::info!("{}, {}", module, func);
-                            let start = node.start_position();
-                            let end = node.end_position();
-                            results.push({
-                                let name = String::from(func);
-                                let deprecated = None;
-                                let location = {
-                                    let uri = uri.clone();
-                                    let range = {
-                                        let start = Position::new(start.row as u64, start.column as u64);
-                                        let end = Position::new(end.row as u64, end.column as u64);
-                                        Range::new(start, end)
-                                    };
-                                    Location { uri, range }
-                                };
-                                let container_name = Some(String::from(module));
-                                let kind = SymbolKind::Function;
-                                SymbolInformation {
-                                    name,
-                                    kind,
-                                    deprecated,
-                                    location,
-                                    container_name,
-                                }
-                            });
-                        }
+                if pattern_index == queries::document_symbol::patterns::FUNC
+                    && state[pattern_index].wrapping_rem_euclid(2) == 0
+                {
+                    if let [module, func] = stack[pattern_index].drain(.. 2).collect::<Vec<_>>()[..] {
+                        log::info!("{}, {}", module, func);
+                        results.push({
+                            SymbolInformation {
+                                name: String::from(func),
+                                kind: SymbolKind::Function,
+                                deprecated: Default::default(),
+                                location: crate::lsp::node::location(uri.clone(), &node),
+                                container_name: Some(String::from(module)),
+                            }
+                        });
                     }
                 }
             }
+
             response = Some(DocumentSymbolResponse::Flat(results));
         }
     }
