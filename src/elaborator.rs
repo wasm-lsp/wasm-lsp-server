648a44e4d70cea955f2de20aeba3dbbed436eb97
Darin Morrison <darinmorrison@users.noreply.github.com>
2020-04-02 13:37:00 -0600

    Process remaining module fields; fix #22

--- src/elaborator.rs
+++ src/elaborator.rs
@@ -77,14 +77,32 @@ pub async fn document_symbol(
         let language = tree.language();
 
         let kind_COMMAND = language.id_for_node_kind("command", true);
+        let kind_DATA = language.id_for_node_kind("data", true);
+        let kind_ELEM = language.id_for_node_kind("elem", true);
         let kind_ENTRYPOINT = language.id_for_node_kind("ENTRYPOINT", true);
         let kind_FUNC = language.id_for_node_kind("func", true);
+        let kind_GLOBAL = language.id_for_node_kind("global", true);
+        let kind_MEMORY = language.id_for_node_kind("mem", true);
         let kind_MODULE = language.id_for_node_kind("module", true);
+        let kind_TABLE = language.id_for_node_kind("table", true);
         let kind_TYPE = language.id_for_node_kind("type", true);
 
         let field_COMMAND = language.field_id_for_name("command").unwrap();
         let field_FIELD = language.field_id_for_name("field").unwrap();
         let field_ID = language.field_id_for_name("id").unwrap();
+
+        let modulefield_filter = |node: &tree_sitter::Node| {
+            [
+                kind_DATA,
+                kind_ELEM,
+                kind_FUNC,
+                kind_GLOBAL,
+                kind_MEMORY,
+                kind_TABLE,
+                kind_TYPE,
+            ]
+            .contains(&node.kind_id())
+        };
 
         while let Some(next) = work.pop() {
             match next {
@@ -131,6 +149,38 @@ pub async fn document_symbol(
                     work.push(Work::Node(command));
                 },
 
+                Work::Node(node) if node.kind_id() == kind_DATA => {
+                    let NameAndRanges {
+                        name,
+                        range,
+                        selection_range,
+                    } = crate::lsp::node::name_and_ranges(&document.text.as_bytes(), &node, field_ID);
+                    work.push(Work::Data);
+                    data.push(Data {
+                        children_count: 0,
+                        kind: SymbolKind::Key,
+                        name,
+                        range,
+                        selection_range,
+                    });
+                },
+
+                Work::Node(node) if node.kind_id() == kind_ELEM => {
+                    let NameAndRanges {
+                        name,
+                        range,
+                        selection_range,
+                    } = crate::lsp::node::name_and_ranges(&document.text.as_bytes(), &node, field_ID);
+                    work.push(Work::Data);
+                    data.push(Data {
+                        children_count: 0,
+                        kind: SymbolKind::Field,
+                        name,
+                        range,
+                        selection_range,
+                    });
+                },
+
                 Work::Node(node) if node.kind_id() == kind_FUNC => {
                     let NameAndRanges {
                         name,
@@ -141,6 +191,38 @@ pub async fn document_symbol(
                     data.push(Data {
                         children_count: 0,
                         kind: SymbolKind::Function,
+                        name,
+                        range,
+                        selection_range,
+                    });
+                },
+
+                Work::Node(node) if node.kind_id() == kind_GLOBAL => {
+                    let NameAndRanges {
+                        name,
+                        range,
+                        selection_range,
+                    } = crate::lsp::node::name_and_ranges(&document.text.as_bytes(), &node, field_ID);
+                    work.push(Work::Data);
+                    data.push(Data {
+                        children_count: 0,
+                        kind: SymbolKind::Event,
+                        name,
+                        range,
+                        selection_range,
+                    });
+                },
+
+                Work::Node(node) if node.kind_id() == kind_MEMORY => {
+                    let NameAndRanges {
+                        name,
+                        range,
+                        selection_range,
+                    } = crate::lsp::node::name_and_ranges(&document.text.as_bytes(), &node, field_ID);
+                    work.push(Work::Data);
+                    data.push(Data {
+                        children_count: 0,
+                        kind: SymbolKind::Array,
                         name,
                         range,
                         selection_range,
@@ -158,7 +240,7 @@ pub async fn document_symbol(
                     let mut children_count = 0;
                     for modulefield in node
                         .children_by_field_id(field_FIELD, &mut node.walk())
-                        .filter(|node| node.kind_id() == kind_FUNC || node.kind_id() == kind_TYPE)
+                        .filter(modulefield_filter)
                     {
                         work.push(Work::Node(modulefield));
                         children_count += 1;
@@ -167,6 +249,22 @@ pub async fn document_symbol(
                     data.push(Data {
                         children_count,
                         kind: SymbolKind::Module,
+                        name,
+                        range,
+                        selection_range,
+                    });
+                },
+
+                Work::Node(node) if node.kind_id() == kind_TABLE => {
+                    let NameAndRanges {
+                        name,
+                        range,
+                        selection_range,
+                    } = crate::lsp::node::name_and_ranges(&document.text.as_bytes(), &node, field_ID);
+                    work.push(Work::Data);
+                    data.push(Data {
+                        children_count: 0,
+                        kind: SymbolKind::Interface,
                         name,
                         range,
                         selection_range,
