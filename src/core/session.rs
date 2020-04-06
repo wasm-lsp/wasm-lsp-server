177c8654458bc31cfa41493d376fe577aac2c193
silvanshade <silvanshade@users.noreply.github.com>
2020-04-06 16:41:59 -0600

    Use a timeout for getting a document

--- src/core/session.rs
+++ src/core/session.rs
@@ -11,6 +11,8 @@ use dashmap::{
 };
 use futures::stream::{self, StreamExt};
 use lsp_types::*;
+use std::time::Duration;
+use tokio::time::timeout;
 use zerocopy::AsBytes;
 
 /// Represents the current state of the LSP service.
@@ -51,54 +53,62 @@ impl Session {
     }
 
     /// Get a document from the session. If the document is not yet open, this function will await
-    /// until that happens. This usually occurs by a call to Self::insert_document from another
-    /// thread of control.
-    pub(crate) async fn get_document(&'_ self, uri: &Url) -> Fallible<Option<Ref<'_, Url, Document>>> {
-        let mut result = self.documents.get(uri);
-        if result.is_none() {
-            let subscriber = self.database.trees.documents.watch_prefix(vec![]);
-            let mut stream = stream::iter(subscriber);
-            // FIXME: use a timeout for this in case the event never arrives; should be an error
-            while let Some(event) = stream.next().await {
-                if let sled::Event::Insert(key, val) = event {
-                    if &*key == uri.as_ref().as_bytes() && val == DocumentStatus::opened().as_bytes() {
-                        if let Some(document) = self.documents.get(uri) {
-                            result = Some(document);
-                            break;
-                        } else {
-                            // FIXME
-                            unreachable!()
+    /// until that happens (up to 5 seconds, otherwise failing). This usually occurs by a call to
+    /// Self::insert_document from another thread of control.
+    pub(crate) async fn get_document(&self, uri: &Url) -> Fallible<Option<Ref<'_, Url, Document>>> {
+        #![allow(clippy::needless_lifetimes)]
+        async fn future<'a>(session: &'a Session, uri: &Url) -> Fallible<Option<Ref<'a, Url, Document>>> {
+            let mut result = session.documents.get(uri);
+            if result.is_none() {
+                let subscriber = session.database.trees.documents.watch_prefix(vec![]);
+                let mut stream = stream::iter(subscriber);
+                // FIXME: use a timeout for this in case the event never arrives; should be an error
+                while let Some(event) = stream.next().await {
+                    if let sled::Event::Insert(key, val) = event {
+                        if &*key == uri.as_ref().as_bytes() && val == DocumentStatus::opened().as_bytes() {
+                            if let Some(document) = session.documents.get(uri) {
+                                result = Some(document);
+                                break;
+                            } else {
+                                // FIXME
+                                unreachable!()
+                            }
                         }
                     }
                 }
             }
+            Ok(result)
         }
-        Ok(result)
+        timeout(Duration::from_secs(5), future(self, uri)).await?
     }
 
     /// Get a mutable document from the session. If the document is not yet open, this function will
-    /// await until that happens. This usually occurs by a call to Self::insert_document from
-    /// another thread of control.
-    pub(crate) async fn get_mut_document(&'_ self, uri: &Url) -> Fallible<Option<RefMut<'_, Url, Document>>> {
-        let mut result = self.documents.get_mut(uri);
-        if result.is_none() {
-            let subscriber = self.database.trees.documents.watch_prefix(vec![]);
-            let mut stream = stream::iter(subscriber);
-            // FIXME: use a timeout for this in case the event never arrives; should be an error
-            while let Some(event) = stream.next().await {
-                if let sled::Event::Insert(key, val) = event {
-                    if &*key == uri.as_ref().as_bytes() && val == DocumentStatus::opened().as_bytes() {
-                        if let Some(document) = self.documents.get_mut(uri) {
-                            result = Some(document);
-                            break;
-                        } else {
-                            // FIXME
-                            unreachable!()
+    /// await until that happens (up to 5 seconds, otherwise failing). This usually occurs by a call
+    /// to Self::insert_document from another thread of control.
+    pub(crate) async fn get_mut_document(&self, uri: &Url) -> Fallible<Option<RefMut<'_, Url, Document>>> {
+        #![allow(clippy::needless_lifetimes)]
+        async fn future<'a>(session: &'a Session, uri: &Url) -> Fallible<Option<RefMut<'a, Url, Document>>> {
+            let mut result = session.documents.get_mut(uri);
+            if result.is_none() {
+                let subscriber = session.database.trees.documents.watch_prefix(vec![]);
+                let mut stream = stream::iter(subscriber);
+                // FIXME: use a timeout for this in case the event never arrives; should be an error
+                while let Some(event) = stream.next().await {
+                    if let sled::Event::Insert(key, val) = event {
+                        if &*key == uri.as_ref().as_bytes() && val == DocumentStatus::opened().as_bytes() {
+                            if let Some(document) = session.documents.get_mut(uri) {
+                                result = Some(document);
+                                break;
+                            } else {
+                                // FIXME
+                                unreachable!()
+                            }
                         }
                     }
                 }
             }
+            Ok(result)
         }
-        Ok(result)
+        timeout(Duration::from_secs(5), future(self, uri)).await?
     }
 }
