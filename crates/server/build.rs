84c73a5ec7bdeccb0270efaad9f0309007e402df
Darin Morrison <darinmorrison@users.noreply.github.com>
2020-07-26 16:51:02 -0600

    Re-run build script if parser artifacts change

--- crates/server/build.rs
+++ crates/server/build.rs
@@ -4,26 +4,35 @@ type Error = Box<dyn std::error::Error + 'static>;
 
 fn compile_tree_sitter_grammars() -> Result<(), Error> {
     let dir = Path::new("../../vendor/tree-sitter-wasm");
+
+    println!("cargo:rerun-if-changed={:?}", dir.join("wast/src/parser.c"));
     cc::Build::new()
         .include(dir.join("wast/src"))
         .file(dir.join("wast/src/parser.c"))
         .flag("-Wno-overflow")
         .compile("tree-sitter-wast");
+
+    println!("cargo:rerun-if-changed={:?}", dir.join("wat/src/parser.c"));
     cc::Build::new()
         .include(dir.join("wat/src"))
         .file(dir.join("wat/src/parser.c"))
         .flag("-Wno-overflow")
         .compile("tree-sitter-wat");
+
+    println!("cargo:rerun-if-changed={:?}", dir.join("wit/src/parser.c"));
     cc::Build::new()
         .include(dir.join("wit/src"))
         .file(dir.join("wit/src/parser.c"))
         .flag("-Wno-overflow")
         .compile("tree-sitter-wit");
+
+    println!("cargo:rerun-if-changed={:?}", dir.join("witx/src/parser.c"));
     cc::Build::new()
         .include(dir.join("witx/src"))
         .file(dir.join("witx/src/parser.c"))
         .flag("-Wno-overflow")
         .compile("tree-sitter-witx");
+
     Ok(())
 }
 
