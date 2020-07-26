fd7719a823bb42bcc4ef09d14c65b8515946e5d8
Darin Morrison <darinmorrison@users.noreply.github.com>
2020-07-26 12:45:50 -0600

    Ignore error nodes in comments; fix #42

--- crates/server/src/core/language.rs
+++ crates/server/src/core/language.rs
@@ -47,6 +47,8 @@ pub(crate) mod wast {
 
         lazy_static! {
             pub(crate) static ref COMMAND: u16 = super::language().id_for_node_kind("command", true);
+            pub(crate) static ref COMMENT_BLOCK: u16 = super::language().id_for_node_kind("comment_block", true);
+            pub(crate) static ref COMMENT_LINE: u16 = super::language().id_for_node_kind("comment_line", true);
             pub(crate) static ref INLINE_MODULE: u16 = super::language().id_for_node_kind("inline_module", true);
             pub(crate) static ref MODULE_FIELD_DATA: u16 =
                 super::language().id_for_node_kind("module_field_data", true);
@@ -88,6 +90,8 @@ pub(crate) mod wat {
 
         lazy_static! {
             pub(crate) static ref INLINE_MODULE: u16 = super::language().id_for_node_kind("inline_module", true);
+            pub(crate) static ref COMMENT_BLOCK: u16 = super::language().id_for_node_kind("comment_block", true);
+            pub(crate) static ref COMMENT_LINE: u16 = super::language().id_for_node_kind("comment_line", true);
             pub(crate) static ref MODULE_FIELD_DATA: u16 =
                 super::language().id_for_node_kind("module_field_data", true);
             pub(crate) static ref MODULE_FIELD_ELEM: u16 =
