acc4d9dfbd5de4e8a4cb7418450049ace91f24a9
Darin Morrison <darinmorrison@users.noreply.github.com>
2020-07-25 18:28:36 -0600

    Use a procedural macro to generate corpus tests

---
+++ crates/macros/src/lib.rs
@@ -0,0 +1,137 @@
+#![allow(dead_code)]
+#![allow(unused_imports)]
+
+use glob::glob;
+use proc_macro::TokenStream;
+use quote::ToTokens;
+use syn::{
+    parse::{Parse, ParseStream},
+    punctuated::Punctuated,
+};
+
+struct MacroInput {
+    corpus: syn::Ident,
+    pattern: String,
+    ignore: Vec<String>,
+}
+
+impl Parse for MacroInput {
+    fn parse(input: ParseStream) -> syn::parse::Result<Self> {
+        let content;
+        let corpus = input.parse()?;
+        input.parse::<syn::Token![,]>()?;
+        let pattern = input.parse::<syn::LitStr>()?.value();
+        input.parse::<syn::Token![,]>()?;
+        syn::bracketed!(content in input);
+        let ignore = content.parse_terminated::<syn::LitStr, syn::Token![,]>(|b| b.parse())?;
+        let ignore = ignore.into_iter().map(|s| s.value()).collect();
+        Ok(MacroInput {
+            corpus,
+            pattern,
+            ignore,
+        })
+    }
+}
+
+#[proc_macro]
+pub fn corpus_tests(input: TokenStream) -> TokenStream {
+    let MacroInput {
+        corpus,
+        pattern,
+        ignore,
+    } = syn::parse_macro_input!(input as MacroInput);
+    let entries = glob(&pattern).unwrap();
+    let mut content = Vec::<syn::Item>::new();
+    for entry in entries {
+        let path = entry.unwrap().canonicalize().unwrap();
+        let path_name = path.to_str().unwrap();
+        let file_name = path.file_name().unwrap().to_str().unwrap();
+        if !ignore.contains(&String::from(file_name)) {
+            let file_stem = path.file_stem().unwrap().to_str().unwrap();
+            let extension = path.extension().and_then(std::ffi::OsStr::to_str);
+            if let Some("wast") = extension {
+                let test_name = str::replace(file_stem, "-", "_");
+                let test_name = format!("_{}", test_name);
+                let test_name = syn::parse_str::<syn::Ident>(&test_name).unwrap();
+                let item: syn::Item = syn::parse_quote! {
+                    #[tokio::test]
+                    async fn #test_name() -> anyhow::Result<()> {
+                        let uri = Url::from_file_path(&#path_name).unwrap();
+                        let text = std::fs::read_to_string(#path_name).unwrap();
+
+                        let (mut service, mut messages) = test::service::spawn()?;
+                        let service = &mut service;
+
+                        assert_ready!(service, Ok(()));
+                        let request = &json!({
+                            "jsonrpc": "2.0",
+                            "method": "initialize",
+                            "params": {
+                                "capabilities":{},
+                            },
+                            "id": 1,
+                        });
+                        let response = Some(json!({
+                            "jsonrpc": "2.0",
+                            "result": {
+                                "capabilities": {
+                                    "documentSymbolProvider": true,
+                                    "textDocumentSync": {
+                                        "change": TextDocumentSyncKind::Full,
+                                        "openClose": true,
+                                    },
+                                },
+                            },
+                            "id": 1,
+                        }));
+                        assert_exchange!(service, request, Ok(response));
+
+                        assert_ready!(service, Ok(()));
+                        let request = &json!({
+                            "jsonrpc": "2.0",
+                            "method": "textDocument/didOpen",
+                            "params": {
+                                "textDocument": {
+                                    "uri": uri,
+                                    "languageId": "wasm.wast",
+                                    "version": 1,
+                                    "text": text,
+                                },
+                            },
+                        });
+                        let response = None::<Value>;
+                        assert_exchange!(service, request, Ok(response));
+
+                        let message = messages.next().await.unwrap();
+                        let actual = serde_json::from_str::<Value>(&message)?;
+                        let expected = json!({
+                            "jsonrpc": "2.0",
+                            "method": "textDocument/publishDiagnostics",
+                            "params": {
+                                "uri": uri,
+                                "diagnostics": [],
+                            },
+                        });
+                        assert_eq!(actual, expected);
+
+                        Ok(())
+                    }
+                };
+                content.push(item);
+            }
+        }
+    }
+
+    let module: syn::ItemMod = syn::parse_quote! {
+        mod #corpus {
+            use futures::stream::StreamExt;
+            use serde_json::{json, Value};
+            use std::task::Poll;
+            use tower_lsp::lsp_types::*;
+            use wasm_language_server::test;
+
+            #(#content)*
+        }
+    };
+    module.to_token_stream().into()
+}
---
+++ crates/server/tests/lib.rs
@@ -0,0 +1,137 @@
+#[allow(unused_imports)]
+#[macro_use]
+extern crate wasm_language_server;
+
+#[cfg(feature = "test")]
+mod lsp {
+    use serde_json::{json, Value};
+    use std::task::Poll;
+    use tower_lsp::{jsonrpc, lsp_types::*, ExitedError};
+    use wasm_language_server::test;
+
+    #[tokio::test]
+    async fn initialize_once() -> anyhow::Result<()> {
+        let service = &mut test::service::spawn()?.0;
+
+        let request = &json!({
+            "jsonrpc": "2.0",
+            "method": "initialize",
+            "params": {
+                "capabilities":{},
+            },
+            "id": 1,
+        });
+
+        // expect nominal response for first request
+        assert_ready!(service, Ok(()));
+        let response = Some(json!({
+            "jsonrpc": "2.0",
+            "result": {
+                "capabilities": {
+                    "documentSymbolProvider": true,
+                    "textDocumentSync": {
+                        "change": TextDocumentSyncKind::Full,
+                        "openClose": true,
+                    },
+                },
+            },
+            "id": 1,
+        }));
+        assert_exchange!(service, request, Ok(response));
+
+        // expect error response for second request
+        assert_ready!(service, Ok(()));
+        let response = Some(json!({
+            "jsonrpc": "2.0",
+            "error": {
+                "code": jsonrpc::ErrorCode::InvalidRequest.code(),
+                "message": "Invalid request",
+            },
+            "id": 1,
+        }));
+        assert_exchange!(service, request, Ok(response));
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn initialize() -> anyhow::Result<()> {
+        let service = &mut test::service::spawn()?.0;
+
+        assert_ready!(service, Ok(()));
+        let request = &json!({
+            "jsonrpc": "2.0",
+            "method": "initialize",
+            "params": {
+                "capabilities":{},
+            },
+            "id": 1,
+        });
+        let response = Some(json!({
+            "jsonrpc": "2.0",
+            "result": {
+                "capabilities": {
+                    "documentSymbolProvider": true,
+                    "textDocumentSync": {
+                        "change": TextDocumentSyncKind::Full,
+                        "openClose": true,
+                    },
+                },
+            },
+            "id": 1,
+        }));
+        assert_exchange!(service, request, Ok(response));
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn exit() -> anyhow::Result<()> {
+        let service = &mut test::service::spawn()?.0;
+
+        assert_ready!(service, Ok(()));
+        let request = &json!({ "jsonrpc": "2.0", "method": "initialized" });
+        let response = None::<Value>;
+        assert_exchange!(service, request, Ok(response));
+
+        assert_ready!(service, Ok(()));
+        let request = &json!({ "jsonrpc": "2.0", "method": "exit" });
+        let response = None::<Value>;
+        assert_exchange!(service, request, Ok(response));
+
+        assert_ready!(service, Err(ExitedError));
+        let request = &json!({ "jsonrpc": "2.0", "method": "initialized" });
+        let error = ExitedError;
+        assert_exchange!(service, request, Err(error));
+
+        Ok(())
+    }
+
+    mod text_document {
+        mod did_open {
+            use wasm_language_server_macros::corpus_tests;
+
+            corpus_tests!(
+                annotations,
+                "vendor/corpus/vendor/WebAssembly/annotations/test/core/*.wast",
+                // FIXME
+                [
+                    "annotations.wast",
+                    "comments.wast",
+                    "conversions.wast",
+                    "f32.wast",
+                    "f64.wast",
+                    "float_exprs.wast",
+                    "float_misc.wast"
+                ]
+            );
+
+            corpus_tests!(
+                spec,
+                "vendor/corpus/vendor/WebAssembly/spec/test/core/*.wast",
+                // FIXME
+                ["comments.wast"]
+            );
+        }
+    }
+}
