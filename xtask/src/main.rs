76d10d2d8a827d332d25fa8a6f31813745c4154e
Darin Morrison <darinmorrison@users.noreply.github.com>
2020-08-13 08:52:51 -0600

    Add --with-corpus option for xtask init; fix #53

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -26,7 +26,11 @@ fn main() -> anyhow::Result<()> {
         clap::SubCommand::with_name("clippy"),
         clap::SubCommand::with_name("doc"),
         clap::SubCommand::with_name("format"),
-        clap::SubCommand::with_name("init"),
+        clap::SubCommand::with_name("init").arg(
+            clap::Arg::with_name("with-corpus")
+                .long("with-corpus")
+                .help("Initialize corpus submodules"),
+        ),
         clap::SubCommand::with_name("install"),
         clap::SubCommand::with_name("test"),
     ]);
@@ -87,24 +91,32 @@ fn main() -> anyhow::Result<()> {
         cmd.status()?;
     }
 
-    if matches.subcommand_matches("init").is_some() {
-        let paths = &["vendor/corpus", "vendor/tree-sitter-wasm"];
+    if let Some(matches) = matches.subcommand_matches("init") {
+        // initialize "vendor/tree-sitter-wasm" submodule
+        let submodule = std::path::Path::new("vendor/tree-sitter-wasm").to_str().unwrap();
+        let mut cmd = std::process::Command::new("git");
+        cmd.current_dir(metadata::project_root());
+        cmd.args(&["submodule", "update", "--init", "--depth", "1", "--", submodule]);
+        cmd.status()?;
 
-        for path in paths {
-            let submodule = std::path::Path::new(path).to_str().unwrap();
+        if matches.is_present("with-corpus") {
+            // initialize "vendor/corpus" submodule
+            let submodule = std::path::Path::new("vendor/corpus").to_str().unwrap();
             let mut cmd = std::process::Command::new("git");
             cmd.current_dir(metadata::project_root());
             cmd.args(&["submodule", "update", "--init", "--depth", "1", "--", submodule]);
             cmd.status()?;
-        }
 
-        let mut cmd = std::process::Command::new("git");
-        let root = metadata::project_root();
-        let root = root.to_str().unwrap();
-        let path = [root, "vendor", "corpus"].iter().collect::<std::path::PathBuf>();
-        cmd.current_dir(path);
-        cmd.args(&["submodule", "update", "--init", "--depth", "1"]);
-        cmd.status()?;
+            // initialize "vendor/corpus/..." submodules
+            let mut cmd = std::process::Command::new("git");
+            let root = metadata::project_root();
+            let path = [root.to_str().unwrap(), "vendor", "corpus"]
+                .iter()
+                .collect::<std::path::PathBuf>();
+            cmd.current_dir(path);
+            cmd.args(&["submodule", "update", "--init", "--depth", "1"]);
+            cmd.status()?;
+        }
     }
 
     if matches.subcommand_matches("install").is_some() {
