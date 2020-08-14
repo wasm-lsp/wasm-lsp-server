b9f6e6900566c3dc0847a22cd29ebd9ace8315b8
silvanshade <silvanshade@users.noreply.github.com>
2020-08-13 18:15:22 -0600

    Regenerate parsers on demand; fix #54

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -4,6 +4,43 @@
 #![deny(missing_docs)]
 #![deny(unsafe_code)]
 
+fn main() -> anyhow::Result<()> {
+    env_logger::try_init()?;
+
+    let app = clap::App::new("xtask").subcommands(vec![
+        clap::SubCommand::with_name("check"),
+        clap::SubCommand::with_name("clippy"),
+        clap::SubCommand::with_name("doc"),
+        clap::SubCommand::with_name("format"),
+        clap::SubCommand::with_name("init").arg(
+            clap::Arg::with_name("with-corpus")
+                .long("with-corpus")
+                .help("Initialize corpus submodules"),
+        ),
+        clap::SubCommand::with_name("install").arg(
+            clap::Arg::with_name("rebuild-parsers")
+                .long("rebuild-parsers")
+                .help("Rebuild tree-sitter parsers if necessary"),
+        ),
+        clap::SubCommand::with_name("test"),
+    ]);
+
+    let matches = app.get_matches_safe()?;
+
+    match matches.subcommand_name() {
+        Some("check") => subcommand::cargo::check()?,
+        Some("clippy") => subcommand::cargo::clippy()?,
+        Some("doc") => subcommand::cargo::doc()?,
+        Some("format") => subcommand::cargo::format()?,
+        Some("init") => subcommand::init(&matches)?,
+        Some("install") => subcommand::cargo::install(&matches)?,
+        Some("test") => subcommand::cargo::test()?,
+        _ => {},
+    }
+
+    Ok(())
+}
+
 mod metadata {
     use std::path::{Path, PathBuf};
 
@@ -25,7 +62,11 @@ mod metadata {
 mod subcommand {
     pub mod cargo {
         use crate::metadata;
-        use std::process::Command;
+        use std::{
+            fs,
+            path::PathBuf,
+            process::{Command, Stdio},
+        };
 
         // Run `cargo check` with custom options.
         pub fn check() -> anyhow::Result<()> {
@@ -90,19 +131,99 @@ mod subcommand {
         }
 
         // Run `cargo install` with custom options.
-        pub fn install() -> anyhow::Result<()> {
+        pub fn install(matches: &clap::ArgMatches) -> anyhow::Result<()> {
+            if let Some(matches) = matches.subcommand_matches("install") {
+                if matches.is_present("rebuild-parsers") {
+                    // Configure the project root path.
+                    let root_path = metadata::project_root();
+                    let root_path = root_path.to_str().unwrap();
+
+                    // Configure the tree-sitter directory path.
+                    let tree_sitter_path = [root_path, "vendor", "tree-sitter-wasm"].iter().collect::<PathBuf>();
+                    let tree_sitter_path = tree_sitter_path.to_str().unwrap();
+
+                    // Configure the tree-sitter cli binary path.
+                    let tree_sitter_cli_path = [tree_sitter_path, "node_modules", ".bin", "tree-sitter"]
+                        .iter()
+                        .collect::<PathBuf>();
+                    let tree_sitter_cli_path = tree_sitter_cli_path.to_str().unwrap();
+
+                    // Check if the tree-sitter cli binary is available.
+                    let mut cmd;
+                    if cfg!(target_os = "windows") {
+                        cmd = Command::new("cmd");
+                        cmd.args(&["/C", format!("{} --help", tree_sitter_cli_path).as_ref()]);
+                    } else {
+                        cmd = Command::new("sh");
+                        cmd.args(&["-c", format!("{} --help", tree_sitter_cli_path).as_ref()]);
+                    };
+                    cmd.stdout(Stdio::null());
+                    cmd.stderr(Stdio::null());
+
+                    // Run `npm ci` first if `tree-sitter` binary is not available.
+                    if !cmd.status()?.success() {
+                        log::info!("installing tree-sitter toolchain");
+                        let mut cmd;
+                        if cfg!(target_os = "windows") {
+                            cmd = Command::new("cmd");
+                            cmd.args(&["/C", "npm ci"]);
+                        } else {
+                            cmd = Command::new("sh");
+                            cmd.args(&["-c", "npm ci"]);
+                        }
+                        cmd.current_dir(tree_sitter_path);
+                        cmd.stdout(Stdio::null());
+                        cmd.stderr(Stdio::null());
+                        cmd.status()?;
+                    }
+
+                    // Iterate through the different grammar types.
+                    for grammar in &["wast", "wat", "wit", "witx"] {
+                        // Configure the grammar directory path.
+                        let grammar_path = [tree_sitter_path, grammar].iter().collect::<PathBuf>();
+                        let grammar_path = fs::canonicalize(grammar_path)?;
+                        let grammar_path = grammar_path.to_str().unwrap();
+
+                        // Configure the grammar.js tree-sitter grammar definition path.
+                        let js_path = [grammar_path, "grammar.js"].iter().collect::<PathBuf>();
+                        let js_date = fs::metadata(js_path)?.modified()?;
+
+                        // Configure the parser.c tree-sitter generated parser path.
+                        let cc_path = [grammar_path, "src", "parser.c"].iter().collect::<PathBuf>();
+                        let cc_date = fs::metadata(cc_path)?.modified()?;
+
+                        // Check if the modification date on the grammar definition is newer than the generated parser.
+                        // If it is, we want to regenerate the parser from the updated grammar.
+                        if cc_date.duration_since(js_date).is_err() {
+                            log::info!("regenerating parser: {}", grammar);
+                            let commands = format!("cd {} && {} generate", grammar_path, tree_sitter_cli_path);
+                            let mut cmd;
+                            if cfg!(target_os = "windows") {
+                                cmd = Command::new("cmd");
+                                cmd.args(&["/C", commands.as_ref()]);
+                            } else {
+                                cmd = Command::new("sh");
+                                cmd.args(&["-c", commands.as_ref()]);
+                            }
+                            cmd.status()?;
+                        }
+                    }
+                }
+            }
+
             let cargo = metadata::cargo()?;
-            let mut cmd = std::process::Command::new(cargo);
+            let mut cmd = Command::new(cargo);
             cmd.current_dir(metadata::project_root());
             cmd.args(&["install", "--path", "crates/server", "--offline"]);
             cmd.status()?;
+
             Ok(())
         }
 
         // Run `cargo test` with custom options.
         pub fn test() -> anyhow::Result<()> {
             let cargo = metadata::cargo()?;
-            let mut cmd = std::process::Command::new(cargo);
+            let mut cmd = Command::new(cargo);
             cmd.current_dir(metadata::project_root());
             cmd.env("RUSTFLAGS", "-Dwarnings");
             cmd.args(&[
@@ -147,7 +268,8 @@ mod subcommand {
                 // initialize "vendor/corpus/..." submodules
                 let mut cmd = Command::new("git");
                 let root = metadata::project_root();
-                let path = [root.to_str().unwrap(), "vendor", "corpus"].iter().collect::<PathBuf>();
+                let root = root.to_str().unwrap();
+                let path = [root, "vendor", "corpus"].iter().collect::<PathBuf>();
                 cmd.current_dir(path);
                 cmd.args(&["submodule", "update", "--init", "--depth", "1"]);
                 cmd.status()?;
@@ -156,35 +278,4 @@ mod subcommand {
 
         Ok(())
     }
-}
-
-fn main() -> anyhow::Result<()> {
-    let app = clap::App::new("xtask").subcommands(vec![
-        clap::SubCommand::with_name("check"),
-        clap::SubCommand::with_name("clippy"),
-        clap::SubCommand::with_name("doc"),
-        clap::SubCommand::with_name("format"),
-        clap::SubCommand::with_name("init").arg(
-            clap::Arg::with_name("with-corpus")
-                .long("with-corpus")
-                .help("Initialize corpus submodules"),
-        ),
-        clap::SubCommand::with_name("install"),
-        clap::SubCommand::with_name("test"),
-    ]);
-
-    let matches = app.get_matches_safe()?;
-
-    match matches.subcommand_name() {
-        Some("check") => subcommand::cargo::check()?,
-        Some("clippy") => subcommand::cargo::clippy()?,
-        Some("doc") => subcommand::cargo::doc()?,
-        Some("format") => subcommand::cargo::format()?,
-        Some("init") => subcommand::init(&matches)?,
-        Some("install") => subcommand::cargo::install()?,
-        Some("test") => subcommand::cargo::test()?,
-        _ => {},
-    }
-
-    Ok(())
 }
