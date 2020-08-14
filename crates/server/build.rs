b306bf8ec328f98c510fbb58c9e542e1591b2d3a
Darin Morrison <darinmorrison@users.noreply.github.com>
2020-08-14 13:08:45 -0600

    Use flag_if_supported rather than if expression

--- crates/server/build.rs
+++ crates/server/build.rs
@@ -7,38 +7,30 @@ fn compile_tree_sitter_grammars() -> Result<(), Error> {
 
     println!("cargo:rerun-if-changed={:?}", dir.join("wast/src/parser.c"));
     let mut cc = cc::Build::new();
-    if !(cc.get_compiler().is_like_msvc()) {
-        cc.flag("-Wno-overflow");
-    }
     cc.include(dir.join("wast/src"));
     cc.file(dir.join("wast/src/parser.c"));
+    cc.flag_if_supported("-Wno-overflow");
     cc.compile("tree-sitter-wast");
 
     println!("cargo:rerun-if-changed={:?}", dir.join("wat/src/parser.c"));
     let mut cc = cc::Build::new();
-    if !(cc.get_compiler().is_like_msvc()) {
-        cc.flag("-Wno-overflow");
-    }
     cc.include(dir.join("wat/src"));
     cc.file(dir.join("wat/src/parser.c"));
+    cc.flag_if_supported("-Wno-overflow");
     cc.compile("tree-sitter-wat");
 
     println!("cargo:rerun-if-changed={:?}", dir.join("wit/src/parser.c"));
     let mut cc = cc::Build::new();
-    if !(cc.get_compiler().is_like_msvc()) {
-        cc.flag("-Wno-overflow");
-    }
     cc.include(dir.join("wit/src"));
     cc.file(dir.join("wit/src/parser.c"));
+    cc.flag_if_supported("-Wno-overflow");
     cc.compile("tree-sitter-wit");
 
     println!("cargo:rerun-if-changed={:?}", dir.join("witx/src/parser.c"));
     let mut cc = cc::Build::new();
-    if !(cc.get_compiler().is_like_msvc()) {
-        cc.flag("-Wno-overflow");
-    }
     cc.include(dir.join("witx/src"));
     cc.file(dir.join("witx/src/parser.c"));
+    cc.flag_if_supported("-Wno-overflow");
     cc.compile("tree-sitter-witx");
 
     Ok(())
