7bf416da0af4c3f318b39dab719febe1b4d3ac7c
Eyal Kalderon <ebkalderon@gmail.com>
2020-08-20 21:07:34 +0800

    Upgrade to tower-lsp 0.13.0

--- crates/server/src/lsp/api.rs
+++ crates/server/src/lsp/api.rs
@@ -15,7 +15,7 @@ impl LanguageServer for Server {
     }
 
     async fn initialized(&self, _: InitializedParams) {
-        self.client.log_message(MessageType::Info, "server initialized!");
+        self.client.log_message(MessageType::Info, "server initialized!").await;
     }
 
     async fn shutdown(&self) -> Result<()> {
