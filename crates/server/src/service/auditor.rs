245646b7d89bbc55549712bf63adbd0a388b2a6e
Eyal Kalderon <ebkalderon@gmail.com>
2020-08-20 21:23:02 +0800

    Wrap tree-sitter Node<'_> in scope to keep future Send
    It appears that certain `tree-sitter` values are not considered `Send`
    (thread-safe) when retained across an `await` point. To fix this, we
    wrap `node` in a scope so that the variable is destroyed before reaching
    the `client.publish_diagnostics()` call, which must be `Send`.

--- crates/server/src/service/auditor.rs
+++ crates/server/src/service/auditor.rs
@@ -14,83 +14,86 @@ pub(crate) mod tree {
     pub(crate) async fn change(session: Arc<Session>, uri: Url) -> Fallible<()> {
         if let Some(document) = session.get_document(&uri).await? {
             let tree = document.tree.lock().await.clone();
-            let node = tree.root_node();
-            let mut diagnostics = vec![];
-            if node.has_error() {
-                // prepare a query to match tree-sitter ERROR nodes
-                let language = tree.language();
-                let source = "((ERROR) @error)"; // query the tree for ERROR nodes
-                let query = tree_sitter::Query::new(language, source).map_err(Error::TreeSitterQueryError)?;
+            let diagnostics = {
+                let mut diagnostics = vec![];
+                let node = tree.root_node();
+                if node.has_error() {
+                    // prepare a query to match tree-sitter ERROR nodes
+                    let language = tree.language();
+                    let source = "((ERROR) @error)"; // query the tree for ERROR nodes
+                    let query = tree_sitter::Query::new(language, source).map_err(Error::TreeSitterQueryError)?;
 
-                // prepare a query cursor
-                let mut query_cursor = tree_sitter::QueryCursor::new();
-                let text_callback = |node: tree_sitter::Node| &document.text[node.byte_range()];
-                let matches = query_cursor.matches(&query, node, text_callback);
+                    // prepare a query cursor
+                    let mut query_cursor = tree_sitter::QueryCursor::new();
+                    let text_callback = |node: tree_sitter::Node| &document.text[node.byte_range()];
+                    let matches = query_cursor.matches(&query, node, text_callback);
 
-                // iterate the query cursor and construct appropriate lsp diagnostics
-                for tree_sitter::QueryMatch { captures, .. } in matches {
-                    'captures: for tree_sitter::QueryCapture { node, .. } in captures {
-                        // create a cursor node starting from the capture node
-                        let mut cursor = *node;
+                    // iterate the query cursor and construct appropriate lsp diagnostics
+                    for tree_sitter::QueryMatch { captures, .. } in matches {
+                        'captures: for tree_sitter::QueryCapture { node, .. } in captures {
+                            // create a cursor node starting from the capture node
+                            let mut cursor = *node;
 
-                        // traverse upward through the parent nodes
-                        'cursor: while let Some(parent) = cursor.parent() {
-                            cursor = parent;
-                            // ignore further processing if the first non-ERROR
-                            // parent node is a comment node; we do this in
-                            // order to avoid syntax errors due to encoding
-                            // issues (see "comments.wast" and issue #42)
-                            if !cursor.is_error() {
-                                match document.language {
-                                    Language::Wast
-                                        if [
-                                            *wast::kind::COMMENT_BLOCK_ANNOT,
-                                            *wast::kind::COMMENT_BLOCK,
-                                            *wast::kind::COMMENT_LINE_ANNOT,
-                                            *wast::kind::COMMENT_LINE,
-                                        ]
-                                        .contains(&parent.kind_id()) =>
-                                    {
-                                        break 'captures;
+                            // traverse upward through the parent nodes
+                            'cursor: while let Some(parent) = cursor.parent() {
+                                cursor = parent;
+                                // ignore further processing if the first non-ERROR
+                                // parent node is a comment node; we do this in
+                                // order to avoid syntax errors due to encoding
+                                // issues (see "comments.wast" and issue #42)
+                                if !cursor.is_error() {
+                                    match document.language {
+                                        Language::Wast
+                                            if [
+                                                *wast::kind::COMMENT_BLOCK_ANNOT,
+                                                *wast::kind::COMMENT_BLOCK,
+                                                *wast::kind::COMMENT_LINE_ANNOT,
+                                                *wast::kind::COMMENT_LINE,
+                                            ]
+                                            .contains(&parent.kind_id()) =>
+                                        {
+                                            break 'captures;
+                                        }
+                                        Language::Wat
+                                            if [
+                                                *wat::kind::COMMENT_BLOCK_ANNOT,
+                                                *wat::kind::COMMENT_BLOCK,
+                                                *wat::kind::COMMENT_LINE_ANNOT,
+                                                *wat::kind::COMMENT_LINE,
+                                            ]
+                                            .contains(&parent.kind_id()) =>
+                                        {
+                                            break 'captures;
+                                        }
+                                        _ => {
+                                            break 'cursor;
+                                        },
                                     }
-                                    Language::Wat
-                                        if [
-                                            *wat::kind::COMMENT_BLOCK_ANNOT,
-                                            *wat::kind::COMMENT_BLOCK,
-                                            *wat::kind::COMMENT_LINE_ANNOT,
-                                            *wat::kind::COMMENT_LINE,
-                                        ]
-                                        .contains(&parent.kind_id()) =>
-                                    {
-                                        break 'captures;
-                                    }
-                                    _ => {
-                                        break 'cursor;
-                                    },
                                 }
                             }
-                        }
 
-                        let start = node.start_position();
-                        let end = node.end_position();
-                        diagnostics.push({
-                            let range = {
-                                let start = Position::new(start.row as u64, start.column as u64);
-                                let end = Position::new(end.row as u64, end.column as u64);
-                                Range::new(start, end)
-                            };
-                            let severity = Some(DiagnosticSeverity::Error);
-                            let code = None;
-                            let source = Some(String::from("wasm-lsp"));
-                            let message = String::from("syntax error");
-                            let related_information = None;
-                            let tags = None;
-                            Diagnostic::new(range, severity, code, source, message, related_information, tags)
-                        });
+                            let start = node.start_position();
+                            let end = node.end_position();
+                            diagnostics.push({
+                                let range = {
+                                    let start = Position::new(start.row as u64, start.column as u64);
+                                    let end = Position::new(end.row as u64, end.column as u64);
+                                    Range::new(start, end)
+                                };
+                                let severity = Some(DiagnosticSeverity::Error);
+                                let code = None;
+                                let source = Some(String::from("wasm-lsp"));
+                                let message = String::from("syntax error");
+                                let related_information = None;
+                                let tags = None;
+                                Diagnostic::new(range, severity, code, source, message, related_information, tags)
+                            });
+                        }
                     }
                 }
-            }
-            // NOTE: else let elaborator handle
+                // NOTE: else let elaborator handle
+                diagnostics
+            };
             let version = None;
             session.client.publish_diagnostics(uri, diagnostics, version).await;
         }
