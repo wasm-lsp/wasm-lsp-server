77686b818957be77e7733d569e69e08d827808af
silvanshade <silvanshade@users.noreply.github.com>
2020-08-22 15:48:00 -0600

    Formatting

--- crates/server/src/lib.rs
+++ crates/server/src/lib.rs
@@ -6,14 +6,19 @@
 
 // Command-line interface for the WASM language server.
 pub mod cli;
+
 // Core functionality for the WASM language server.
 mod core;
+
 // Functionality related to implementation of the Language Server Protocol (LSP).
 pub mod lsp;
+
 // Definitions related to the wasm-language-server crate.
 mod package;
+
 // Services (components) of the WASM language server.
 mod service;
+
 // Various utility functionality, e.g., for handling lsp or tree-sitter data.
 mod util;
 
