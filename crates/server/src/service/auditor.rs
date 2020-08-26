beba9b51fbc06870e5c631d33453aa834069b6d8
Darin Morrison <darinmorrison@users.noreply.github.com>
2020-08-26 17:03:37 -0600

    Use wast parser for reporting diagnostics

--- crates/server/src/service/auditor.rs
+++ crates/server/src/service/auditor.rs
@@ -5,73 +5,59 @@ pub(crate) mod tree {
     use crate::core::{document::Document, session::Session};
     use std::sync::Arc;
     use tower_lsp::lsp_types::*;
-    use wasm_language_server_parsers::core::language::{wast, wat, Language};
-    use wasm_language_server_shared::core::error::{Error, Fallible};
+    use wasm_language_server_shared::core::error::Fallible;
 
     // Compute diagnostics for a change event given a `document` and `tree`.
-    // NOTE: This function was split out from `change` below in order to avoid
-    // the need to create an explicit scope around the `node` variable since it
-    // must be dropped before the use of `.await`. See PR #61 for details.
+    // NOTE: Currently we only use the tree-sitter grammar to check for the
+    // presence of errors and then use the `wast` crate for the actual error
+    // reporting (because tree-sitter does not provide detailed errors yet).
     fn diagnostics_for_change(document: &Document, tree: tree_sitter::Tree) -> Fallible<Vec<Diagnostic>> {
         let mut diagnostics = vec![];
         let node = tree.root_node();
 
         if node.has_error() {
-            // prepare a query to match tree-sitter ERROR nodes
-            let language = tree.language();
-            let source = "((ERROR) @error)"; // query the tree for ERROR nodes
-            let query = tree_sitter::Query::new(language, source).map_err(Error::TreeSitterQueryError)?;
+            let source = &document.text;
+            let result = ::wast::parser::ParseBuffer::new(source);
 
-            // prepare a query cursor
-            let mut query_cursor = tree_sitter::QueryCursor::new();
-            let text_callback = |node: tree_sitter::Node| &document.text[node.byte_range()];
-            let matches = query_cursor.matches(&query, node, text_callback);
+            match result {
+                Err(error) => diagnostics.push({
+                    let span = error.span();
+                    let (line, col) = span.linecol_in(source);
+                    let range = {
+                        let start = Position::new(line as u64, col as u64);
+                        let end = Position::new(line as u64, col as u64);
+                        Range::new(start, end)
+                    };
+                    let severity = Some(DiagnosticSeverity::Error);
+                    let code = Default::default();
+                    let source = Some(String::from("wast"));
+                    let message = error.message();
+                    let related_information = Default::default();
+                    let tags = Default::default();
+                    Diagnostic::new(range, severity, code, source, message, related_information, tags)
+                }),
+                Ok(buffer) => {
+                    let result = ::wast::parser::parse::<::wast::Wast>(&buffer);
 
-            // iterate the query cursor and construct appropriate lsp diagnostics
-            for tree_sitter::QueryMatch { captures, .. } in matches {
-                'captures: for tree_sitter::QueryCapture { node, .. } in captures {
-                    // create a cursor node starting from the capture node
-                    let mut cursor = *node;
-
-                    // traverse upward through the parent nodes
-                    'cursor: while let Some(parent) = cursor.parent() {
-                        cursor = parent;
-                        // ignore further processing if the first non-ERROR
-                        // parent node is a comment node; we do this in
-                        // order to avoid syntax errors due to encoding
-                        // issues (see "comments.wast" and issue #42)
-                        if !cursor.is_error() {
-                            match document.language {
-                                Language::Wast if wast::kind::is_comment(&parent.kind_id()) => {
-                                    break 'captures;
-                                },
-                                Language::Wat if wat::kind::is_comment(&parent.kind_id()) => {
-                                    break 'captures;
-                                },
-                                _ => {
-                                    break 'cursor;
-                                },
-                            }
-                        }
+                    if let Err(error) = result {
+                        diagnostics.push({
+                            let span = error.span();
+                            let (line, col) = span.linecol_in(source);
+                            let range = {
+                                let start = Position::new(line as u64, col as u64);
+                                let end = Position::new(line as u64, col as u64);
+                                Range::new(start, end)
+                            };
+                            let severity = Some(DiagnosticSeverity::Error);
+                            let code = Default::default();
+                            let source = Some(String::from("wast"));
+                            let message = error.message();
+                            let related_information = Default::default();
+                            let tags = Default::default();
+                            Diagnostic::new(range, severity, code, source, message, related_information, tags)
+                        })
                     }
-
-                    diagnostics.push({
-                        let start = node.start_position();
-                        let end = node.end_position();
-                        let range = {
-                            let start = Position::new(start.row as u64, start.column as u64);
-                            let end = Position::new(end.row as u64, end.column as u64);
-                            Range::new(start, end)
-                        };
-                        let severity = Some(DiagnosticSeverity::Error);
-                        let code = Default::default();
-                        let source = Some(String::from("wasm-lsp"));
-                        let message = String::from("syntax error");
-                        let related_information = Default::default();
-                        let tags = Default::default();
-                        Diagnostic::new(range, severity, code, source, message, related_information, tags)
-                    });
-                }
+                },
             }
         } else {
             // NOTE: else let elaborator handle
