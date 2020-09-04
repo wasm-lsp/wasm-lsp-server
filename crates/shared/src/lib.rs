16e3674116248562e228b096e2176e8a8de44060
Darin Morrison <darinmorrison@users.noreply.github.com>
2020-09-04 15:55:27 -0600

    Update comments

--- crates/shared/src/lib.rs
+++ crates/shared/src/lib.rs
@@ -1,8 +1,8 @@
-//! The WebAssembly language server.
+//! Shared definitions for the wasm-lsp crates.
 
 #![deny(clippy::all)]
 #![deny(missing_docs)]
 #![deny(unsafe_code)]
 
-// LSP-related definitions for the WebAssembly language server.
+/// LSP-related definitions.
 pub mod lsp;
