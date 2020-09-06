27e92d99b861fe97c67195940e00f310aea62ee2
Darin Morrison <darinmorrison@users.noreply.github.com>
2020-09-05 19:29:19 -0600

    Add cli tests

---
+++ crates/server/tests/all/cli.rs
@@ -0,0 +1,62 @@
+const BIN: &str = env!("CARGO_PKG_NAME");
+
+mod invoke {
+    use crate::cli::BIN;
+    use assert_cmd::Command;
+
+    #[test]
+    fn server() -> anyhow::Result<()> {
+        Command::cargo_bin(BIN)?.assert().success();
+        Ok(())
+    }
+
+    mod flag {
+        use crate::cli::BIN;
+        use assert_cmd::Command;
+
+        #[test]
+        fn help() -> anyhow::Result<()> {
+            Command::cargo_bin(BIN)?.arg("--help").assert().success();
+            Ok(())
+        }
+
+        #[test]
+        fn version() -> anyhow::Result<()> {
+            Command::cargo_bin(BIN)?.arg("--version").assert().success();
+            Ok(())
+        }
+    }
+
+    mod server {
+        mod stdio {
+            use crate::cli::BIN;
+            use assert_cmd::Command;
+            use predicates::prelude::*;
+            use wasm_language_server_testing as testing;
+
+            #[test]
+            fn initialize() -> anyhow::Result<()> {
+                let request = testing::lsp::initialize::request().to_string();
+                let response = testing::lsp::initialize::response().to_string();
+                let stdin = format!("Content-Length: {}\r\n\r\n{}", request.len(), request);
+                let stdout = format!("Content-Length: {}\r\n\r\n{}", response.len(), response);
+                Command::cargo_bin(BIN)?.write_stdin(stdin).assert().stdout(stdout);
+                Ok(())
+            }
+
+            #[test]
+            fn newline() -> anyhow::Result<()> {
+                #[rustfmt::skip]
+                const STDOUT: &str = "Content-Length: 75\r\n\r\n{\"jsonrpc\":\"2.0\",\"error\":{\"code\":-32700,\"message\":\"Parse error\"},\"id\":null}";
+                #[rustfmt::skip]
+                const STDERR: &str = "ERROR tower_lsp::transport] failed to decode message: missing required `Content-Length` header\n";
+                Command::cargo_bin(BIN)?
+                    .write_stdin("\n")
+                    .assert()
+                    .stdout(STDOUT)
+                    .stderr(predicate::str::ends_with(STDERR));
+                Ok(())
+            }
+        }
+    }
+}
