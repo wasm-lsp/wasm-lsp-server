157c006c94244293ad51661a5dd954238bc075b1
silvanshade <silvanshade@users.noreply.github.com>
2020-09-07 15:06:03 -0600

    Refactor; move some stuff out of util

--- crates/server/src/util/mod.rs
+++ crates/server/src/util/mod.rs
@@ -2,8 +2,6 @@
 
 /// Functions for creation of lsp data from tree-sitter nodes.
 pub(crate) mod node {
-    use std::borrow::Cow;
-
     /// Functions for creation of lsp position data from tree-sitter nodes.
     mod position {
         /// Creates an lsp position from the starting position of a tree-sitter node.
@@ -22,47 +20,5 @@ pub(crate) mod node {
     /// Creates an lsp range from the range of a tree-sitter node.
     pub(crate) fn range(node: &tree_sitter::Node) -> tower_lsp::lsp_types::Range {
         tower_lsp::lsp_types::Range::new(position::start(node), position::end(node))
-    }
-
-    /// Convenience type for packaging a (symbol) name with an lsp range and selection range.
-    #[derive(Clone, Debug)]
-    pub(crate) struct SymbolRange<'a> {
-        /// The name (identifier) of the symbol.
-        pub(crate) name: Cow<'a, str>,
-        /// The (node-enclosing) range of the symbol.
-        pub(crate) range: tower_lsp::lsp_types::Range,
-        /// The (identifier-enclosing) range of the symbol.
-        pub(crate) selection_range: tower_lsp::lsp_types::Range,
-    }
-
-    /// Compute the name and ranges for a document symbol given tree-sitter node data.
-    pub(crate) fn symbol_range<'a>(
-        source: &'a [u8],
-        empty_name: &'a str,
-        node: &tree_sitter::Node,
-        field_id: u16,
-    ) -> SymbolRange<'a> {
-        let name;
-        let range = crate::util::node::range(&node);
-        let selection_range;
-        if let Some(inner_node) = node.child_by_field_id(field_id) {
-            name = inner_node.utf8_text(source).unwrap().into();
-            selection_range = crate::util::node::range(&inner_node);
-        } else {
-            name = format!(
-                "<{}@{}:{}>",
-                empty_name,
-                range.start.line + 1,
-                range.start.character + 1
-            )
-            .into();
-            selection_range = range;
-        }
-
-        SymbolRange {
-            name,
-            range,
-            selection_range,
-        }
     }
 }
