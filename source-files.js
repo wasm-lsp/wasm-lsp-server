ad35116502b309041d3a3af7c7325278dff7c915
darinmorrison <darinmorrison@users.noreply.github.com>
2020-09-08 01:13:03 +0000

    deploy: 9c05d55654d77240c852f4fb5a21ac0fbbb1f758

--- source-files.js
+++
@@ -1,3 +0,0 @@
-var N = null;var sourcesIndex = {};
-sourcesIndex["wasm_language_server"] = {"name":"","dirs":[{"name":"core","files":["database.rs","document.rs","error.rs","mod.rs","session.rs"]},{"name":"lsp","files":["api.rs","mod.rs","server.rs"]},{"name":"service","dirs":[{"name":"elaborator","files":["mod.rs","wast.rs","wit.rs","witx.rs"]}],"files":["analyzer.rs","auditor.rs","highlighter.rs","mod.rs","synchronizer.rs","synthesizer.rs"]},{"name":"util","files":["mod.rs"]}],"files":["cli.rs","lib.rs","package.rs"]};
-createSourceSidebar();
