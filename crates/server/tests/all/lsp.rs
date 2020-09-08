979c8bd8d69176e446d673db0a595ade3a6d07cb
silvanshade <silvanshade@users.noreply.github.com>
2020-09-08 03:52:07 -0600

    Add another "textDocument/documentSymbols" test

--- crates/server/tests/all/lsp.rs
+++ crates/server/tests/all/lsp.rs
@@ -501,6 +501,67 @@ mod text_document {
     }
 
     #[tokio::test]
+    async fn document_symbol_command() -> anyhow::Result<()> {
+        let uri = Url::parse("inmemory:///test")?;
+        let language_id = "wasm.wast";
+        #[rustfmt::skip]
+        let text = String::from(r#"(assert_return (invoke "empty"))"#);
+
+        let (ref mut service, ref mut messages) = testing::service::spawn()?;
+
+        // send "initialize" request
+        testing::assert_status!(service, Ok(()));
+        let request = &testing::lsp::initialize::request();
+        let response = Some(testing::lsp::initialize::response());
+        testing::assert_exchange!(service, request, Ok(response));
+
+        // send "initialized" notification
+        testing::assert_status!(service, Ok(()));
+        let notification = &testing::lsp::initialized::notification();
+        let status = None::<Value>;
+        testing::assert_exchange!(service, notification, Ok(status));
+        // ignore the "window/logMessage" notification: "WebAssembly language server initialized!"
+        messages.next().await.unwrap();
+
+        // send "textDocument/didOpen" notification for `uri`
+        testing::assert_status!(service, Ok(()));
+        let notification = &testing::lsp::text_document::did_open::notification(&uri, language_id, 1, text);
+        let status = None::<Value>;
+        testing::assert_exchange!(service, notification, Ok(status));
+
+        // receive "textDocument/publishDiagnostics" notification for `uri`
+        let message = messages.next().await.unwrap();
+        let actual = serde_json::to_value(&message)?;
+        let expected = testing::lsp::text_document::publish_diagnostics::notification(&uri, &[]);
+        assert_eq!(actual, expected);
+
+        // send "textDocument/documentSymbol" request for `uri`
+        testing::assert_status!(service, Ok(()));
+        let request = &testing::lsp::text_document::document_symbol::request(&uri);
+        #[rustfmt::skip]
+        let response = Some(json!({
+            "jsonrpc": "2.0",
+            "result": [],
+            "id": 1,
+        }));
+        testing::assert_exchange!(service, request, Ok(response));
+
+        // send "shutdown" request
+        testing::assert_status!(service, Ok(()));
+        let request = &testing::lsp::shutdown::request();
+        let response = Some(testing::lsp::shutdown::response());
+        testing::assert_exchange!(service, request, Ok(response));
+
+        // send "exit" notification
+        testing::assert_status!(service, Ok(()));
+        let notification = &testing::lsp::exit::notification();
+        let status = None::<Value>;
+        testing::assert_exchange!(service, notification, Ok(status));
+
+        Ok(())
+    }
+
+    #[tokio::test]
     async fn hover() -> anyhow::Result<()> {
         let uri = Url::parse("inmemory:///test")?;
         let language_id = "wasm.wast";
