e4a7864223a56f1ff8f358978f8d848c67a98071
silvanshade <silvanshade@users.noreply.github.com>
2020-09-10 16:02:03 -0600

    Add error for when Parser::parse returns None

--- crates/parsers/src/core/error.rs
+++ crates/parsers/src/core/error.rs
@@ -9,6 +9,9 @@ pub enum Error {
     /// Error that occurs when parsing an invalid language-id string.
     #[error("InvalidLanguageId: {0}")]
     InvalidLanguageId(String),
+    /// Error that occurs when `tree_sitter::Parser::parse` returns `None`.
+    #[error("tree_sitter::ParseNone")]
+    TreeSitterParseNone,
     /// Error that occurs when attempting to set an invalid language for a tree-sitter parser.
     #[error("tree_sitter::LanguageError: {0}")]
     TreeSitterLanguageError(tree_sitter::LanguageError),
