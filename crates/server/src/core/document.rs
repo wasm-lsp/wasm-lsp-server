1fc3cdc7c296d7b46d3d8370a3ab5b2ffa26efed
Darin Morrison <darinmorrison@users.noreply.github.com>
2020-09-10 16:02:22 -0600

    Define Document::new

--- crates/server/src/core/document.rs
+++ crates/server/src/core/document.rs
@@ -1,17 +1,35 @@
 //! Core functionality related to documents.
 
 use crate::core::language::Language;
+use std::convert::TryFrom;
 use tokio::sync::Mutex;
 use tree_sitter::{Parser, Tree};
 
 /// The current state of a document.
-pub(crate) struct Document {
+pub struct Document {
     /// The language type of the document, e.g., "wasm.wast"
-    pub(crate) language: Language,
+    pub language: Language,
     /// The tree-sitter parser state for the document.
-    pub(crate) parser: Mutex<Parser>,
+    pub parser: Mutex<Parser>,
     /// The current text of the document.
-    pub(crate) text: String,
+    pub text: String,
     /// The current tree-sitter parse tree of the document.
-    pub(crate) tree: Mutex<Tree>,
+    pub tree: Mutex<Tree>,
+}
+
+impl Document {
+    /// Create a new Document for the given `language_id` and `text`.
+    pub fn new(language_id: &str, text: String) -> anyhow::Result<Self> {
+        use wasm_language_server_parsers::core::error::Error::TreeSitterParseNone;
+        let language = Language::try_from(language_id)?;
+        let mut parser = tree_sitter::Parser::try_from(language)?;
+        let old_tree = None;
+        let tree = parser.parse(&text[..], old_tree).ok_or(TreeSitterParseNone)?;
+        Ok(Document {
+            language,
+            parser: Mutex::new(parser),
+            text: text.clone(),
+            tree: Mutex::new(tree),
+        })
+    }
 }
