5f66a0605f6763cf1d8e8c062e203d825a7ae8b8
Darin Morrison <darinmorrison@users.noreply.github.com>
2020-09-11 17:11:28 -0600

    Add a benchmark for "textDocument/hover"

--- crates/server/benches/all/main.rs
+++ crates/server/benches/all/main.rs
@@ -68,9 +68,50 @@ mod document_symbol {
     }
 }
 
+mod hover {
+    pub mod spec {
+        use criterion::Criterion;
+        use std::{convert::TryFrom, path::Path};
+        use tower_lsp::lsp_types::*;
+        use wasm_language_server::{core::document::Document, service};
+        use wasm_language_server_parsers::core::language::Language;
+
+        pub fn float_exprs(c: &mut Criterion) {
+            let name = format!("{}/float_exprs.wast", crate::corpus::SPEC);
+            let path = Path::new(&name);
+            let path = path.canonicalize().unwrap();
+            let uri = Url::from_file_path(&path).unwrap();
+            let language = Language::try_from(path.as_path()).unwrap();
+            let text = std::fs::read_to_string(path).unwrap();
+
+            let document = Document::new(language.id(), text).unwrap().unwrap();
+            let params = HoverParams {
+                text_document_position_params: TextDocumentPositionParams {
+                    text_document: TextDocumentIdentifier { uri },
+                    position: Position {
+                        line: 676,
+                        character: 45,
+                    },
+                },
+                work_done_progress_params: Default::default(),
+            };
+
+            let mut runtime = tokio::runtime::Builder::new().basic_scheduler().build().unwrap();
+
+            c.bench_function("hover::spec::float_exprs.wast", |b| {
+                b.iter(|| {
+                    runtime
+                        .block_on(service::analyzer::hover_with_document(&document, &params))
+                        .unwrap();
+                })
+            });
+        }
+    }
+}
+
 criterion_group! {
     name = benches;
     config = Criterion::default();
-    targets = document_symbol::spec::all, document_symbol::spec::float_exprs
+    targets = document_symbol::spec::all, document_symbol::spec::float_exprs, hover::spec::float_exprs
 }
 criterion_main!(benches);
