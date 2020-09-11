adf30f0cb6a5caf1767cb14b0e8c5beb9eab6eaa
Darin Morrison <darinmorrison@users.noreply.github.com>
2020-09-11 15:58:20 -0600

    Add TryFrom<Path> impl

--- crates/parsers/src/core/error.rs
+++ crates/parsers/src/core/error.rs
@@ -1,5 +1,6 @@
 //! Core definitions related to runtime errors.
 
+use std::path::PathBuf;
 use thiserror::Error;
 
 /// Runtime errors for the WebAssembly parsers.
@@ -9,6 +10,12 @@ pub enum Error {
     /// Error that occurs when parsing an invalid language-id string.
     #[error("InvalidLanguageId: {0}")]
     InvalidLanguageId(String),
+    /// Error that occurs when `OsStr::to_str()` returns `None`.
+    #[error("OsStrToStrFailed")]
+    OsStrToStrFailed,
+    /// Error that occurs when `Path::extension()` returns `None`.
+    #[error("PathExtensionFailed: {0}")]
+    PathExtensionFailed(PathBuf),
     /// Error that occurs when attempting to set an invalid language for a tree-sitter parser.
     #[error("tree_sitter::LanguageError: {0}")]
     TreeSitterLanguageError(tree_sitter::LanguageError),
