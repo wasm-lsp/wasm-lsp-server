324befac07204ec70435dfad4326b622c8173b37
silvanshade <silvanshade@users.noreply.github.com>
2020-11-12 10:49:49 -0700

    Add semantic tokens capability configuration

--- crates/server/src/lsp/server.rs
+++ crates/server/src/lsp/server.rs
@@ -26,6 +26,19 @@ pub fn capabilities() -> ServerCapabilities {
 
     let hover_provider = Some(HoverProviderCapability::Simple(true));
 
+    let semantic_tokens_provider = {
+        let options = SemanticTokensOptions {
+            work_done_progress_options: Default::default(),
+            legend: SemanticTokensLegend {
+                token_types: Default::default(),
+                token_modifiers: Default::default(),
+            },
+            range: Default::default(),
+            full: Some(SemanticTokensFullOptions::Bool(true)),
+        };
+        Some(SemanticTokensServerCapabilities::SemanticTokensOptions(options))
+    };
+
     let text_document_sync = {
         let options = TextDocumentSyncOptions {
             open_close: Some(true),
@@ -38,6 +51,7 @@ pub fn capabilities() -> ServerCapabilities {
     ServerCapabilities {
         document_symbol_provider,
         hover_provider,
+        semantic_tokens_provider,
         text_document_sync,
         ..Default::default()
     }
