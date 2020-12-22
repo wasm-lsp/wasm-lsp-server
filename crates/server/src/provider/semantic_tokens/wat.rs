2d5afa5f48b6de2ab5d1a85f4813dc37b92e5106
Darin Morrison <darinmorrison@users.noreply.github.com>
2020-12-21 18:20:50 -0700

    Refactoring

--- crates/server/src/provider/semantic_tokens/wat.rs
+++ crates/server/src/provider/semantic_tokens/wat.rs
@@ -1,122 +1,132 @@
 //! Semantic tokens provider definitions for ".wast" files.
 
-pub(crate) mod full {
-    use crate::core::{document::Document, session::Session};
-    use lspower::lsp_types::*;
-    use std::sync::Arc;
+use crate::{
+    core::{document::Document, language::wat, session::Session},
+    provider::semantic_tokens::SemanticTokensBuilder,
+};
+use anyhow::anyhow;
+use lspower::lsp_types::*;
+use std::{collections::VecDeque, sync::Arc};
 
-    pub(crate) async fn response(
-        session: Arc<Session>,
-        document: &Document,
-        params: SemanticTokensParams,
-    ) -> anyhow::Result<Option<SemanticTokensResult>> {
-        let params = SemanticTokensRangeParams {
-            work_done_progress_params: params.work_done_progress_params,
-            partial_result_params: params.partial_result_params,
-            text_document: params.text_document,
-            range: {
-                let tree = document.tree.lock().await;
-                let node = tree.root_node();
-                crate::util::node::range(&node)
-            },
+pub(crate) async fn full(
+    session: Arc<Session>,
+    document: &Document,
+    params: SemanticTokensParams,
+) -> anyhow::Result<Option<SemanticTokensResult>> {
+    let params = SemanticTokensRangeParams {
+        work_done_progress_params: params.work_done_progress_params,
+        partial_result_params: params.partial_result_params,
+        text_document: params.text_document,
+        range: {
+            let tree = document.tree.lock().await;
+            let node = tree.root_node();
+            crate::util::node::range(&node)
+        },
+    };
+
+    let result = range(session, document, params).await?.map(|result| match result {
+        SemanticTokensRangeResult::Tokens(tokens) => SemanticTokensResult::Tokens(tokens),
+        SemanticTokensRangeResult::Partial(partial) => SemanticTokensResult::Partial(partial),
+    });
+
+    Ok(result)
+}
+
+pub(crate) async fn range(
+    session: Arc<Session>,
+    document: &Document,
+    params: SemanticTokensRangeParams,
+) -> anyhow::Result<Option<SemanticTokensRangeResult>> {
+    let legend = session.semantic_tokens_legend().await;
+
+    if let Some(node) = {
+        let start = tree_sitter::Point {
+            row: params.range.start.line as usize,
+            column: params.range.start.character as usize,
+        };
+        let end = tree_sitter::Point {
+            row: params.range.end.line as usize,
+            column: params.range.end.character as usize,
+        };
+        document
+            .tree
+            .lock()
+            .await
+            .root_node()
+            .descendant_for_point_range(start, end)
+    } {
+        let mut builder = SemanticTokensBuilder::new(legend.as_ref());
+
+        let mut cursor = node.walk();
+        let mut work = {
+            let mut res = VecDeque::new();
+            res.push_back(node);
+            res
         };
 
-        let result = super::range::response(session, document, params)
-            .await?
-            .map(|result| match result {
-                SemanticTokensRangeResult::Tokens(tokens) => SemanticTokensResult::Tokens(tokens),
-                SemanticTokensRangeResult::Partial(partial) => SemanticTokensResult::Partial(partial),
-            });
+        while let Some(this) = work.pop_front() {
+            if wat::kind::equals::MODULE(this.kind_id()) {
+                handle::module(&mut cursor, &mut work, this, &mut builder)?;
+                continue;
+            }
 
-        Ok(result)
+            if wat::kind::equals::ROOT(this.kind_id()) {
+                handle::root(&mut cursor, &mut work, this, &mut builder)?;
+                continue;
+            }
+
+            cursor.reset(this);
+            work.extend(this.named_children(&mut cursor))
+        }
+
+        let tokens = builder.build()?;
+        let result = SemanticTokensRangeResult::Tokens(tokens);
+
+        Ok(Some(result))
+    } else {
+        Err(anyhow!("Could not obtain tree node for given range"))
     }
 }
 
-pub(crate) mod range {
-    use crate::{
-        core::{document::Document, language::wat, session::Session},
-        provider::semantic_tokens::SemanticTokensBuilder,
-    };
-    use anyhow::anyhow;
+mod handle {
+    use crate::{core::language::wat, provider::semantic_tokens::SemanticTokensBuilder};
     use lspower::lsp_types::*;
-    use std::{collections::VecDeque, sync::Arc};
+    use std::collections::VecDeque;
 
-    pub(crate) async fn response(
-        session: Arc<Session>,
-        document: &Document,
-        params: SemanticTokensRangeParams,
-    ) -> anyhow::Result<Option<SemanticTokensRangeResult>> {
-        let legend = session.semantic_tokens_legend().await;
-
-        let module_fields: &[u16] = &[
-            *wat::kind::MODULE_FIELD_DATA,
-            *wat::kind::MODULE_FIELD_ELEM,
-            *wat::kind::MODULE_FIELD_FUNC,
-            *wat::kind::MODULE_FIELD_GLOBAL,
-            *wat::kind::MODULE_FIELD_MEMORY,
-            *wat::kind::MODULE_FIELD_TABLE,
-            *wat::kind::MODULE_FIELD_TYPE,
-        ];
-
-        if let Some(node) = {
-            let start = tree_sitter::Point {
-                row: params.range.start.line as usize,
-                column: params.range.start.character as usize,
-            };
-            let end = tree_sitter::Point {
-                row: params.range.end.line as usize,
-                column: params.range.end.character as usize,
-            };
-            document
-                .tree
-                .lock()
-                .await
-                .root_node()
-                .descendant_for_point_range(start, end)
-        } {
-            let mut builder = SemanticTokensBuilder::new(legend.as_ref());
-
-            let mut cursor = node.walk();
-            let mut work = {
-                let mut res = VecDeque::new();
-                res.push_back(node);
-                res
-            };
-
-            while let Some(this) = work.pop_front() {
-                if *wat::kind::ROOT == this.kind_id() {
-                    cursor.reset(this);
-                    let commands = this
-                        .named_children(&mut cursor)
-                        .filter(|it| [&[*wat::kind::MODULE], module_fields].concat().contains(&it.kind_id()));
-                    work.extend(commands);
-                    continue;
-                }
-
-                if *wat::kind::MODULE == this.kind_id() {
-                    if let Some(node) = this.child(1) {
-                        let range = crate::util::node::range(&node);
-                        builder.push(range, &SemanticTokenType::KEYWORD, None)?;
-                    }
-
-                    cursor.reset(this);
-                    let fields = this
-                        .named_children(&mut cursor)
-                        .filter(|it| module_fields.contains(&it.kind_id()));
-                    work.extend(fields);
-                    continue;
-                }
-
-                cursor.reset(this);
-                work.extend(this.named_children(&mut cursor))
-            }
-
-            let tokens = builder.build()?;
-            let result = SemanticTokensRangeResult::Tokens(tokens);
-
-            Ok(Some(result))
-        } else {
-            Err(anyhow!("Could not obtain tree node for given range"))
+    pub(super) fn module<'a>(
+        cursor: &mut tree_sitter::TreeCursor<'a>,
+        work: &mut VecDeque<tree_sitter::Node<'a>>,
+        this: tree_sitter::Node<'a>,
+        builder: &mut SemanticTokensBuilder<'a>,
+    ) -> anyhow::Result<()> {
+        if let Some(node) = this.child(1) {
+            let range = crate::util::node::range(&node);
+            builder.push(range, &SemanticTokenType::KEYWORD, None)?;
         }
+
+        cursor.reset(this);
+        let fields = this
+            .named_children(cursor)
+            .filter(|it| wat::MODULE_FIELDS.contains(&it.kind_id()));
+        work.extend(fields);
+
+        Ok(())
+    }
+
+    pub(super) fn root<'a>(
+        cursor: &mut tree_sitter::TreeCursor<'a>,
+        work: &mut VecDeque<tree_sitter::Node<'a>>,
+        this: tree_sitter::Node<'a>,
+        _builder: &mut SemanticTokensBuilder<'a>,
+    ) -> anyhow::Result<()> {
+        cursor.reset(this);
+        let commands = this.named_children(cursor).filter(|it| {
+            [&[*wat::kind::MODULE], wat::MODULE_FIELDS.as_slice()]
+                .concat()
+                .contains(&it.kind_id())
+        });
+        work.extend(commands);
+
+        Ok(())
     }
 }
