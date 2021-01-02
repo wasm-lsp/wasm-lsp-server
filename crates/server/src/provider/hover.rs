b962acc93bc556e300353e063c13950de911b5d1
Darin Morrison <darinmorrison@users.noreply.github.com>
2021-01-01 19:41:51 -0700

    Move position conversion functions to util

--- crates/server/src/provider/hover.rs
+++ crates/server/src/provider/hover.rs
@@ -2,78 +2,9 @@
 
 use crate::core::{
     document::Document,
-    error::Error,
     language::{wast, wat},
 };
 use lspower::lsp_types::*;
-
-// FIXME: move to util
-fn character_to_line_offset(line_text: &str, character: usize) -> anyhow::Result<usize> {
-    let mut offset = 0;
-
-    let mut chars = line_text.chars();
-    while let Some(c) = chars.next() {
-        if offset == character {
-            return Ok(line_text.len() - chars.as_str().len() - c.len_utf8());
-        }
-        offset += c.len_utf16();
-    }
-
-    // Handle positions after the last character on the line
-    if offset == character {
-        Ok(line_text.len())
-    } else {
-        Err(Error::ColumnOutOfBounds {
-            given: offset as usize,
-            max: line_text.len(),
-        }
-        .into())
-    }
-}
-
-// FIXME: move to util
-fn line_range(document: &Document, line_index: usize) -> Option<std::ops::Range<usize>> {
-    let (start, end) = line_span(document, line_index).ok()?;
-    Some(start .. end)
-}
-
-// FIXME: move to util
-fn line_span(document: &Document, line_index: usize) -> anyhow::Result<(usize, usize)> {
-    let line_starts = line_starts(document).collect::<Vec<_>>();
-    let this_start = line_start(document, &line_starts, line_index)?;
-    let next_start = line_start(document, &line_starts, line_index + 1)?;
-    Ok((this_start, next_start))
-}
-
-// FIXME: move to util
-fn line_start(document: &Document, line_starts: &[usize], line_index: usize) -> anyhow::Result<usize> {
-    use std::cmp::Ordering;
-    match line_index.cmp(&line_starts.len()) {
-        Ordering::Less => Ok(line_starts[line_index]),
-        Ordering::Equal => Ok(document.text.len()),
-        Ordering::Greater => Err(Error::LineOutOfBounds {
-            given: line_index,
-            max: line_starts.len(),
-        }
-        .into()),
-    }
-}
-
-// FIXME: move to util
-fn line_starts<'a>(document: &'a Document) -> impl 'a + Iterator<Item = usize> {
-    let source = document.text.as_str();
-    std::iter::once(0).chain(source.match_indices('\n').map(|i| i.0 + 1))
-}
-
-// FIXME: move to util
-fn position_to_byte_index(document: &Document, position: &Position) -> anyhow::Result<usize> {
-    let source = document.text.as_str();
-    let line_span: std::ops::Range<usize> = line_range(document, position.line as usize).unwrap();
-    let line_text = source.get(line_span.clone()).unwrap();
-    let character = position.character as usize;
-    let byte_offset = character_to_line_offset(line_text, character)?;
-    Ok(line_span.start + byte_offset)
-}
 
 /// Compute "textDocument/hover" for a given document.
 pub async fn response(document: &Document, params: &HoverParams) -> anyhow::Result<Option<Hover>> {
@@ -110,8 +41,8 @@ async fn hover_for_token_range(_uri: &Url, document: &Document, range: Range) ->
         *wat::kind::MODULE_FIELD_TYPE,
     ];
 
-    let start = position_to_byte_index(document, &range.start)?;
-    let end = position_to_byte_index(document, &range.end)?;
+    let start = crate::util::position::byte_index(document, &range.start)?;
+    let end = crate::util::position::byte_index(document, &range.end)?;
 
     let mut contents = vec![];
     let mut range = None;
@@ -162,14 +93,13 @@ async fn hover_for_token_range(_uri: &Url, document: &Document, range: Range) ->
 
 #[cfg(test)]
 mod tests {
-    use super::*;
     use crate::core::document::Document;
 
     #[test]
     fn character_to_line_offset_ok() {
         let line_text = "text";
         let character = line_text.len();
-        let result = character_to_line_offset(line_text, character);
+        let result = crate::util::character::line::offset(line_text, character);
         assert!(result.is_ok())
     }
 
@@ -177,7 +107,7 @@ mod tests {
     fn character_to_line_offset_out_of_bounds() {
         let line_text = "text";
         let character = line_text.len() + 1;
-        let result = character_to_line_offset(line_text, character);
+        let result = crate::util::character::line::offset(line_text, character);
         assert!(result.is_err())
     }
 
@@ -190,9 +120,9 @@ mod tests {
         if let Ok(option) = result {
             assert!(option.is_some());
             if let Some(ref document) = option {
-                let line_starts = line_starts(document).collect::<Vec<_>>();
+                let line_starts = crate::util::line::starts(document).collect::<Vec<_>>();
                 let line_index = 1;
-                let result = line_start(document, &line_starts, line_index);
+                let result = crate::util::line::start(document, &line_starts, line_index);
                 assert!(result.is_ok())
             }
         }
@@ -207,9 +137,9 @@ mod tests {
         if let Ok(option) = result {
             assert!(option.is_some());
             if let Some(ref document) = option {
-                let line_starts = line_starts(document).collect::<Vec<_>>();
+                let line_starts = crate::util::line::starts(document).collect::<Vec<_>>();
                 let line_index = 2;
-                let result = line_start(document, &line_starts, line_index);
+                let result = crate::util::line::start(document, &line_starts, line_index);
                 assert!(result.is_err())
             }
         }
