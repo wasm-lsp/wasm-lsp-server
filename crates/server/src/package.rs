ae6e1bdb18c688fa41eed2e4e7fcb790c113cdb5
silvanshade <silvanshade@users.noreply.github.com>
2021-01-13 15:41:09 -0700

    Split CLI into separate crate

--- crates/server/src/package.rs
+++ crates/server/src/package.rs
@@ -1,7 +1,7 @@
 //! Definitions related to the wasm-language-server crate.
 
 /// Metadata for the crate computed from the Cargo manifest and repository.
-pub(crate) mod metadata {
+pub mod metadata {
     #![allow(dead_code)]
     include!(concat!(env!("OUT_DIR"), "/built.rs"));
 }
