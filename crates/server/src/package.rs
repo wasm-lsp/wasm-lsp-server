b601ff945997a2faad0726f61290ccd759bb2528
silvanshade <silvanshade@users.noreply.github.com>
2021-01-25 20:26:32 -0700

    Remove comments for now

--- crates/server/src/package.rs
+++ crates/server/src/package.rs
@@ -1,6 +1,3 @@
-//! Definitions related to the wasm-language-server crate.
-
-/// Metadata for the crate computed from the Cargo manifest and repository.
 pub mod metadata {
     #![allow(dead_code)]
     include!(concat!(env!("OUT_DIR"), "/built.rs"));
