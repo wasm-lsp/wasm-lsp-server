49d0b4a0c911b4ed4768109260b796afc63e86db
hvithrafn <hvithrafn@users.noreply.github.com>
2021-02-13 11:25:20 -0700

    Define NodeExt utility trait

--- crates/parsers/src/core/mod.rs
+++ crates/parsers/src/core/mod.rs
@@ -1,3 +1,13 @@
 pub mod error;
 pub mod language;
 pub mod parser;
+
+pub trait NodeExt {
+    fn matches_subtypes(&self, supertype_id: u16, subtype_ids: &[u16]) -> bool;
+}
+
+impl<'tree> NodeExt for tree_sitter::Node<'tree> {
+    fn matches_subtypes(&self, supertype_id: u16, subtype_ids: &[u16]) -> bool {
+        supertype_id == self.kind_id() && subtype_ids.contains(&self.kind_id())
+    }
+}
