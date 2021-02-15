ca64c955497f745f62b462855f54440e5ced35f8
hvithrafn <hvithrafn@users.noreply.github.com>
2021-02-14 18:32:55 -0700

    Rewrite SemanticToeksnBuilder

--- crates/server/src/provider/semantic_tokens/wast.rs
+++ crates/server/src/provider/semantic_tokens/wast.rs
@@ -59,7 +59,7 @@ pub(crate) async fn range(
         let end = range.end_point();
         tree.root_node().descendant_for_point_range(start, end)
     } {
-        let mut handler = Handler::new(content, legend, node);
+        let mut handler = Handler::new(content, legend, node)?;
 
         loop {
             if handler.walker.done {
@@ -231,11 +231,11 @@ impl<'text, 'tree> Handler<'text, 'tree> {
         content: &'text ropey::Rope,
         legend: Option<&'tree lsp::SemanticTokensLegend>,
         node: tree_sitter::Node<'tree>,
-    ) -> Self {
+    ) -> anyhow::Result<Self> {
         let language = Language::Wast;
-        let builder = SemanticTokensBuilder::new(content, legend);
+        let builder = SemanticTokensBuilder::new(content, legend)?;
         let walker = NodeWalker::new(language, node);
-        Self { builder, walker }
+        Ok(Self { builder, walker })
     }
 
     fn action_get(&mut self) -> anyhow::Result<()> {
