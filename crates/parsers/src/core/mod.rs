28cb65adf0bedceda476d40d5a19841cb4e655b8
silvanshade <silvanshade@users.noreply.github.com>
2021-02-17 12:59:18 -0700

    Fix definition of matches_subtype

--- crates/parsers/src/core/mod.rs
+++ crates/parsers/src/core/mod.rs
@@ -8,6 +8,10 @@ pub trait NodeExt {
 
 impl<'tree> NodeExt for tree_sitter::Node<'tree> {
     fn matches_subtypes(&self, supertype_id: u16, subtype_ids: &[u16]) -> bool {
-        supertype_id == self.kind_id() && subtype_ids.contains(&self.kind_id())
+        if let Some(child) = self.named_child(0) {
+            supertype_id == self.kind_id() && subtype_ids.contains(&child.kind_id())
+        } else {
+            false
+        }
     }
 }
