03666359f587a68d6eeacbbe50e1c75e1306c81f
silvanshade <silvanshade@users.noreply.github.com>
2021-02-17 13:02:20 -0700

    Fix block_on call (for full vs lite futures crate)

--- crates/parsers/src/core/language/wat.rs
+++ crates/parsers/src/core/language/wat.rs
@@ -13,7 +13,7 @@ pub fn language() -> tree_sitter::Language {
     let bytes: &[u8] = include_bytes!("../../../../../vendor/tree-sitter-wasm/wat/tree-sitter-wat.wasm");
     let promise = web_tree_sitter_sys::Language::load_bytes(&bytes.into());
     let future = JsFuture::from(promise);
-    let result = futures::future::block_on(future).unwrap();
+    let result = futures::executor::block_on(future).unwrap();
     let inner = result.unchecked_into::<web_tree_sitter_sys::Language>();
     inner.into()
 }
