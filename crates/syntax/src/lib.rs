495960353d61ac95c415c04df4db2a24530ef369
hvithrafn <hvithrafn@users.noreply.github.com>
2021-02-23 14:46:23 -0700

    Refactor NodeExt

--- crates/syntax/src/lib.rs
+++ crates/syntax/src/lib.rs
@@ -11,6 +11,9 @@ pub mod error;
 /// Functionality related to [`tree-sitter::Language`].
 pub mod language;
 
+/// Functionality related to [`tree-sitter::Node`].
+pub mod node;
+
 /// Functionality related to [`tree-sitter::Parser`].
 pub mod parser;
 
@@ -31,20 +34,4 @@ extern {
     #[allow(dead_code)]
     #[doc(hidden)]
     fn tree_sitter_witx() -> tree_sitter_sys::Language;
-}
-
-/// Utility trait for working with [`tree_sitter::Node`].
-pub trait NodeExt {
-    /// Predicate to determine if a supertype node matches a given subtype node kind.
-    fn matches_subtypes(&self, supertype_id: u16, subtype_ids: &[u16]) -> bool;
-}
-
-impl<'tree> NodeExt for tree_sitter::Node<'tree> {
-    fn matches_subtypes(&self, supertype_id: u16, subtype_ids: &[u16]) -> bool {
-        if let Some(child) = self.named_child(0) {
-            supertype_id == self.kind_id() && subtype_ids.contains(&child.kind_id())
-        } else {
-            false
-        }
-    }
 }
